9474035e343520abd7b24616f56faa1ed378e12a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-08-27 19:22:40 +0200

    create bpt_teleport

---
+++ server-data/resources/[esx_addons]/bpt_teleport/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'bitpredator'
+
+description 'bpt_teleport'
+
+version '1.0.0'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua',
+    'warehouses.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua'
+}
---
+++ server-data/resources/[esx_addons]/bpt_teleport/warehouses.lua
@@ -0,0 +1,56 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        local playerCoords = GetEntityCoords(PlayerPedId(), true)
+
+        -- import enter check
+        if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, 1025.393433, -2398.905518, 29.903320, true) <= 3.0 then
+            ESX.ShowHelpNotification(_U('press_to_enter'))
+            if IsControlPressed(0, 51) then
+                DoScreenFadeOut(1000)
+                Citizen.Wait(1500)
+
+                SetEntityCoords(PlayerPedId(), 1022.123108, -2398.377930, 30.122314) 
+
+                Citizen.Wait(1000)
+                DoScreenFadeIn(1000)
+            end
+        end
+
+        -- import exit
+        if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, 1022.123108, -2398.377930, 30.122314, true) <= 3.0 then
+            ESX.ShowHelpNotification(_U('press_to_exit'))
+            if IsControlPressed(0, 51) then
+                DoScreenFadeOut(1000)
+                Citizen.Wait(1500)
+
+                SetEntityCoords(PlayerPedId(), 1025.459351, -2398.865967, 29.903320, 239.736) 
+
+                Citizen.Wait(1000)
+                DoScreenFadeIn(1000)
+            end
+        end
+    end
+end)
+
+
+
+function addBlip(blipX, blipY, blipZ, blipName, blipSprite)
+    local blip = AddBlipForCoord(blipX, blipY, blipZ)
+    SetBlipSprite(blip, blipSprite)
+    SetBlipScale(blip, 1.0)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(blipName)
+    EndTextCommandSetBlipName(blip)
+end
