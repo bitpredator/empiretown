152f8da2c897b03b8aae15a1c684b97b50ccea89
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-09-06 17:51:44 +0200

    refactor(removed some edit points)

--- server-data/resources/[esx_addons]/esx_lscustom/config.lua
+++ server-data/resources/[esx_addons]/esx_lscustom/config.lua
@@ -6,43 +6,7 @@ Config.IsMechanicJobOnly = true
 Config.Zones = {
 
 	ls1 = {
-		Pos   = { x = -337.38, y = -136.92, z = 38.57},
-		Size  = {x = 3.0, y = 3.0, z = 0.2},
-		Color = {r = 204, g = 204, b = 0},
-		Marker= 1,
-		Name  = _U('blip_name'),
-		Hint  = _U('press_custom')
-	},
-
-	ls2 = {
-		Pos   = { x = -1155.53, y = -2007.18, z = 12.74},
-		Size  = {x = 3.0, y = 3.0, z = 0.2},
-		Color = {r = 204, g = 204, b = 0},
-		Marker= 1,
-		Name  = _U('blip_name'),
-		Hint  = _U('press_custom')
-	},
-
-	ls3 = {
-		Pos   = { x = 731.81, y = -1088.82, z = 21.73},
-		Size  = {x = 3.0, y = 3.0, z = 0.2},
-		Color = {r = 204, g = 204, b = 0},
-		Marker= 1,
-		Name  = _U('blip_name'),
-		Hint  = _U('press_custom')
-	},
-
-	ls4 = {
-		Pos   = { x = 1175.04, y = 2640.21, z = 37.32},
-		Size  = {x = 3.0, y = 3.0, z = 0.2},
-		Color = {r = 204, g = 204, b = 0},
-		Marker= 1,
-		Name  = _U('blip_name'),
-		Hint  = _U('press_custom')
-	},
-
-	ls5 = {
-		Pos   = { x = 110.99, y = 6626.39, z = 30.89},
+		Pos   = { x = -338.967041, y = -136.654938, z = 39.002197},
 		Size  = {x = 3.0, y = 3.0, z = 0.2},
 		Color = {r = 204, g = 204, b = 0},
 		Marker= 1,
