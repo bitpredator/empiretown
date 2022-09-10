f955436eae63a75389a4b9eeb778966d331436d1
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-09-10 18:25:10 +0200

    feat(esx_vehicleshop): Reasons For account changes

--- server-data/resources/[esx_addons]/esx_vehicleshop/config.lua
+++ server-data/resources/[esx_addons]/esx_vehicleshop/config.lua
@@ -1,10 +1,10 @@
 Config                            = {}
 Config.DrawDistance               = 10
 Config.MarkerColor                = {r = 120, g = 120, b = 240}
-Config.EnablePlayerManagement     = false -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
+Config.EnablePlayerManagement     = true -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
 Config.ResellPercentage           = 50
 
-Config.Locale                     = 'en'
+Config.Locale                     = 'it'
 
 Config.LicenseEnable = false -- require people to own drivers license when buying vehicles? Only applies if EnablePlayerManagement is disabled. Requires esx_license
 
