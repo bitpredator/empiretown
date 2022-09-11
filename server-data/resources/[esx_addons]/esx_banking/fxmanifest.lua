71c76bd9ecb1925662b40d62d809cfcbc184e43c
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-09-11 10:44:16 +0200

    feat: esx_banking rewrite

--- server-data/resources/[esx_addons]/esx_banking/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_banking/fxmanifest.lua
@@ -1,9 +1,9 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 
 game 'gta5'
 
 description 'ESX banking'
-
+lua54 'yes'
 version '1.7.5'
 
 shared_scripts {
@@ -25,10 +25,7 @@ client_scripts {
 ui_page 'html/ui.html'
 
 files {
-	'html/ui.html',
-	'html/roboto.ttf',
-	'html/css/app.css',
-	'html/scripts/app.js'
+	'html/**',
 }
 
 dependency 'es_extended'
