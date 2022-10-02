bb16eb029ae928ba3e2a751027ff0fbd48116b6a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-10-02 17:30:50 +0200

    feat: (created gunsmith work)

---
+++ server-data/resources/[esx_addons]/bpt_ammujob/bpt_ammujob.sql
@@ -0,0 +1,25 @@
+
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_ammu', 'Ammu', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_ammu', 'Ammu', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_ammu', 'Ammu', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('ammu', 'Armeria')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('ammu',0,'apprentice','Apprendista',20,'{}','{}'),
+	('ammu',1,'gunsmith','Armaiolo',40,'{}','{}'),
+	('ammu',2,'armorychief','Capo Armeria',60,'{}','{}'),
+	('ammu',3,'deputydirector','Vice direttore',85,'{}','{}'),
+	('ammu',4,'boss','Direttore',100,'{}','{}')
+;
---
+++ server-data/resources/[esx_addons]/bpt_ammujob/server/main.lua
@@ -0,0 +1,408 @@
+if Config.EnableESXService then
+	if Config.MaxInService ~= -1 then
+		TriggerEvent('esx_service:activateService', 'ammu', Config.MaxInService)
+	end
+end
+
+TriggerEvent('esx_phone:registerNumber', 'ammu', _U('alert_ammu'), true, true)
+TriggerEvent('esx_society:registerSociety', 'ammu', 'Ammu', 'society_ammu', 'society_ammu', 'society_ammu', {type = 'public'})
+
+RegisterNetEvent('bpt_ammujob:confiscatePlayerItem')
+AddEventHandler('bpt_ammujob:confiscatePlayerItem', function(target, itemType, itemName, amount)
+	local source = source
+	local sourceXPlayer = ESX.GetPlayerFromId(source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+
+	if sourceXPlayer.job.name ~= 'ammu' then
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Exploit The Confuscation System!'):format(sourceXPlayer.source))
+		return
+	end
+
+	if itemType == 'item_standard' then
+		local targetItem = targetXPlayer.getInventoryItem(itemName)
+		local sourceItem = sourceXPlayer.getInventoryItem(itemName)
+
+		-- does the target player have enough in their inventory?
+		if targetItem.count > 0 and targetItem.count <= amount then
+
+			-- can the player carry the said amount of x item?
+			if sourceXPlayer.canCarryItem(itemName, sourceItem.count) then
+				targetXPlayer.removeInventoryItem(itemName, amount)
+				sourceXPlayer.addInventoryItem   (itemName, amount)
+				sourceXPlayer.showNotification(_U('you_confiscated', amount, sourceItem.label, targetXPlayer.name))
+				targetXPlayer.showNotification(_U('got_confiscated', amount, sourceItem.label, sourceXPlayer.name))
+			else
+				sourceXPlayer.showNotification(_U('quantity_invalid'))
+			end
+		else
+			sourceXPlayer.showNotification(_U('quantity_invalid'))
+		end
+
+	elseif itemType == 'item_account' then
+		local targetAccount = targetXPlayer.getAccount(itemName)
+
+		-- does the target player have enough money?
+		if targetAccount.money >= amount then
+			targetXPlayer.removeAccountMoney(itemName, amount, "Confiscated")
+			sourceXPlayer.addAccountMoney   (itemName, amount, "Confiscated")
+
+			sourceXPlayer.showNotification(_U('you_confiscated_account', amount, itemName, targetXPlayer.name))
+			targetXPlayer.showNotification(_U('got_confiscated_account', amount, itemName, sourceXPlayer.name))
+		else
+			sourceXPlayer.showNotification(_U('quantity_invalid'))
+		end
+
+	elseif itemType == 'item_weapon' then
+		if amount == nil then amount = 0 end
+
+		-- does the target player have weapon?
+		if targetXPlayer.hasWeapon(itemName) then
+			targetXPlayer.removeWeapon(itemName)
+			sourceXPlayer.addWeapon   (itemName, amount)
+
+			sourceXPlayer.showNotification(_U('you_confiscated_weapon', ESX.GetWeaponLabel(itemName), targetXPlayer.name, amount))
+			targetXPlayer.showNotification(_U('got_confiscated_weapon', ESX.GetWeaponLabel(itemName), amount, sourceXPlayer.name))
+		else
+			sourceXPlayer.showNotification(_U('quantity_invalid'))
+		end
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:handcuff')
+AddEventHandler('bpt_ammujob:handcuff', function(target)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.job.name == 'ammu' then
+		TriggerClientEvent('bpt_ammujob:handcuff', target)
+	else
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Exploit Handcuffs!'):format(xPlayer.source))
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:drag')
+AddEventHandler('bpt_ammujob:drag', function(target)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.job.name == 'ammu' then
+		TriggerClientEvent('bpt_ammujob:drag', target, source)
+	else
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Exploit Dragging!'):format(xPlayer.source))
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:putInVehicle')
+AddEventHandler('bpt_ammujob:putInVehicle', function(target)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.job.name == 'ammu' then
+		TriggerClientEvent('bpt_ammujob:putInVehicle', target)
+	else
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Exploit Garage!'):format(xPlayer.source))
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:OutVehicle')
+AddEventHandler('bpt_ammujob:OutVehicle', function(target)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.job.name == 'ammu' then
+		TriggerClientEvent('bpt_ammujob:OutVehicle', target)
+	else
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Exploit Dragging Out Of Vehicle!'):format(xPlayer.source))
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:getStockItem')
+AddEventHandler('bpt_ammujob:getStockItem', function(itemName, count)
+	local source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_ammu', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- is there enough in the society?
+		if count > 0 and inventoryItem.count >= count then
+
+			-- can the player carry the said amount of x item?
+			if xPlayer.canCarryItem(itemName, count) then
+				inventory.removeItem(itemName, count)
+				xPlayer.addInventoryItem(itemName, count)
+				xPlayer.showNotification(_U('have_withdrawn', count, inventoryItem.name))
+			else
+				xPlayer.showNotification(_U('quantity_invalid'))
+			end
+		else
+			xPlayer.showNotification(_U('quantity_invalid'))
+		end
+	end)
+end)
+
+RegisterNetEvent('bpt_ammujob:putStockItems')
+AddEventHandler('bpt_ammujob:putStockItems', function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_ammu', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- does the player have enough of the item?
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			xPlayer.showNotification(_U('have_deposited', count, inventoryItem.name))
+		else
+			xPlayer.showNotification(_U('quantity_invalid'))
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:getOtherPlayerData', function(source, cb, target, notify)
+	local xPlayer = ESX.GetPlayerFromId(target)
+
+	if notify then
+		xPlayer.showNotification(_U('being_searched'))
+	end
+
+	if xPlayer then
+		local data = {
+			name = xPlayer.getName(),
+			job = xPlayer.job.label,
+			grade = xPlayer.job.grade_label,
+			inventory = xPlayer.getInventory(),
+			accounts = xPlayer.getAccounts(),
+			weapons = xPlayer.getLoadout()
+		}
+
+		if Config.EnableESXIdentity then
+			data.dob = xPlayer.get('dateofbirth')
+			data.height = xPlayer.get('height')
+
+			if xPlayer.get('sex') == 'm' then data.sex = 'male' else data.sex = 'female' end
+		end
+
+		TriggerEvent('esx_status:getStatus', target, 'drunk', function(status)
+			if status then
+				data.drunk = ESX.Math.Round(status.percent)
+			end
+		end)
+
+		if Config.EnableLicenses then
+			TriggerEvent('esx_license:getLicenses', target, function(licenses)
+				data.licenses = licenses
+				cb(data)
+			end)
+		else
+			cb(data)
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:getVehicleInfos', function(source, cb, plate)
+	local retrivedInfo = {
+		plate = plate
+	}
+	if Config.EnableESXIdentity then
+		MySQL.single('SELECT users.firstname, users.lastname FROM owned_vehicles JOIN users ON owned_vehicles.owner = users.identifier WHERE plate = ?', {plate},
+		function(result)
+			if result then
+				retrivedInfo.owner = ('%s %s'):format(result.firstname, result.lastname)
+			end
+			cb(retrivedInfo)
+		end)
+	else
+		MySQL.scalar('SELECT owner FROM owned_vehicles WHERE plate = ?', {plate},
+		function(owner)
+			if owner then
+				local xPlayer = ESX.GetPlayerFromIdentifier(owner)
+				if xPlayer then
+					retrivedInfo.owner = xPlayer.getName()
+				end
+			end
+			cb(retrivedInfo)
+		end)
+	end
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:getArmoryWeapons', function(source, cb)
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_ammu', function(store)
+		local weapons = store.get('weapons')
+
+		if weapons == nil then
+			weapons = {}
+		end
+
+		cb(weapons)
+	end)
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:addArmoryWeapon', function(source, cb, weaponName, removeWeapon)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if removeWeapon then
+		xPlayer.removeWeapon(weaponName)
+	end
+
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_ammu', function(store)
+		local weapons = store.get('weapons') or {}
+		local foundWeapon = false
+
+		for i=1, #weapons, 1 do
+			if weapons[i].name == weaponName then
+				weapons[i].count = weapons[i].count + 1
+				foundWeapon = true
+				break
+			end
+		end
+
+		if not foundWeapon then
+			table.insert(weapons, {
+				name  = weaponName,
+				count = 1
+			})
+		end
+
+		store.set('weapons', weapons)
+		cb()
+		end)
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:removeArmoryWeapon', function(source, cb, weaponName)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.addWeapon(weaponName, 500)
+
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_ammu', function(store)
+		local weapons = store.get('weapons') or {}
+
+		local foundWeapon = false
+
+		for i=1, #weapons, 1 do
+			if weapons[i].name == weaponName then
+				weapons[i].count = (weapons[i].count > 0 and weapons[i].count - 1 or 0)
+				foundWeapon = true
+				break
+			end
+		end
+
+		if not foundWeapon then
+			table.insert(weapons, {
+				name = weaponName,
+				count = 0
+			})
+		end
+
+		store.set('weapons', weapons)
+		cb()
+	end)
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:buyJobVehicle', function(source, cb, vehicleProps, type)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local price = getPriceFromHash(vehicleProps.model, xPlayer.job.grade_name, type)
+
+	-- vehicle model not found
+	if price == 0 then
+		print(('[^3WARNING^7] Player ^5%s^7 Attempted To Buy Invalid Vehicle - ^5%s^7!'):format(source, vehicleProps.model))
+		cb(false)
+	else
+		if xPlayer.getMoney() >= price then
+			xPlayer.removeMoney(price, "Job Vehicle Bought")
+
+			MySQL.insert('INSERT INTO owned_vehicles (owner, vehicle, plate, type, job, `stored`) VALUES (?, ?, ?, ?, ?, ?)', { xPlayer.identifier, json.encode(vehicleProps), vehicleProps.plate, type, xPlayer.job.name, true},
+			function (rowsChanged)
+				cb(true)
+			end)
+		else
+			cb(false)
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:storeNearbyVehicle', function(source, cb, plates)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	local plate = MySQL.scalar.await('SELECT plate FROM owned_vehicles WHERE owner = ? AND plate IN (?) AND job = ?', {xPlayer.identifier, plates, xPlayer.job.name})
+
+	if plate then
+		MySQL.update('UPDATE owned_vehicles SET `stored` = true WHERE owner = ? AND plate = ? AND job = ?', {xPlayer.identifier, plate, xPlayer.job.name},
+		function(rowsChanged)
+			if rowsChanged == 0 then
+				cb(false)
+			else
+				cb(plate)
+			end
+		end)
+	else
+		cb(false)
+	end
+end)
+
+function getPriceFromHash(vehicleHash, jobGrade, type)
+	local vehicles = Config.AuthorizedVehicles[type][jobGrade]
+
+	for i = 1, #vehicles do
+		local vehicle = vehicles[i]
+		if GetHashKey(vehicle.model) == vehicleHash then
+			return vehicle.price
+		end
+	end
+
+	return 0
+end
+
+ESX.RegisterServerCallback('bpt_ammujob:getStockItems', function(source, cb)
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_ammu', function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+ESX.RegisterServerCallback('bpt_ammujob:getPlayerInventory', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items   = xPlayer.inventory
+
+	cb({items = items})
+end)
+
+AddEventHandler('playerDropped', function()
+	local playerId = source
+	if playerId then
+		local xPlayer = ESX.GetPlayerFromId(playerId)
+
+		if xPlayer and xPlayer.job.name == 'ammu' then
+			Wait(5000)
+			TriggerClientEvent('bpt_ammujob:updateBlip', -1)
+		end
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:spawned')
+AddEventHandler('bpt_ammujob:spawned', function()
+	local playerId = source
+	local xPlayer = ESX.GetPlayerFromId(playerId)
+
+	if xPlayer and xPlayer.job.name == 'ammu' then
+		Wait(5000)
+		TriggerClientEvent('bpt_ammujob:updateBlip', -1)
+	end
+end)
+
+RegisterNetEvent('bpt_ammujob:forceBlip')
+AddEventHandler('bpt_ammujob:forceBlip', function()
+	for _, xPlayer in pairs(ESX.GetExtendedPlayers('job', 'ammu')) do
+		TriggerClientEvent('bpt_ammujob:updateBlip', xPlayer.source)
+	end
+end)
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		Wait(5000)
+		for _, xPlayer in pairs(ESX.GetExtendedPlayers('job', 'ammu')) do
+			TriggerClientEvent('bpt_ammujob:updateBlip', xPlayer.source)
+		end
+	end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		TriggerEvent('esx_phone:removeNumber', 'ammu')
+	end
+end)
