5c6536b43fcae3bed49af88c8878e30c761cf685
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-10-07 21:09:22 +0200

    feat(police radar)

---
+++ server-data/resources/[esx_addons]/wk_wrs/__resource.lua
@@ -0,0 +1,19 @@
+--[[-----------------------------------------------------------------------
+
+    Wraith Radar System - v1.01
+    Created by WolfKnight
+
+-----------------------------------------------------------------------]]--
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page "nui/radar.html"
+
+files {
+	"nui/digital-7.regular.ttf", 
+	"nui/radar.html",
+	"nui/radar.css",
+	"nui/radar.js"
+}
+
+client_script 'cl_radar.lua'
