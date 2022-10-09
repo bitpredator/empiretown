a693693802a18b2e80e5f0b88ee5f12e3b00a450
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-10-09 21:11:53 +0200

    refactor:( corrections and changes to the code)

--- server-data/resources/[esx_addons]/esx_mechanicjob/client/main.lua
+++ server-data/resources/[esx_addons]/esx_mechanicjob/client/main.lua
@@ -48,7 +48,7 @@ function StopNPCJob(cancel)
 	if cancel then
 		ESX.ShowNotification(_U('mission_canceled'), "error")
 	else
-		--TriggerServerEvent('esx_mechanicjob:onNPCJobCompleted')
+		TriggerServerEvent('esx_mechanicjob:onNPCJobCompleted')
 	end
 end
 
@@ -56,9 +56,7 @@ function OpenMechanicActionsMenu()
 	local elements = {
 		{label = _U('vehicle_list'),   value = 'vehicle_list'},
 		{label = _U('work_wear'),      value = 'cloakroom'},
-		{label = _U('civ_wear'),       value = 'cloakroom2'},
-		{label = _U('deposit_stock'),  value = 'put_stock'},
-		{label = _U('withdraw_stock'), value = 'get_stock'}
+		{label = _U('civ_wear'),       value = 'cloakroom2'}
 	}
 
 	if Config.EnablePlayerManagement and ESX.PlayerData.job and ESX.PlayerData.job.grade_name == 'boss' then
@@ -166,8 +164,6 @@ function OpenMechanicActionsMenu()
 		elseif Config.OxInventory and (data.current.value == 'put_stock' or data.current.value == 'get_stock') then
 			exports.ox_inventory:openInventory('stash', 'society_mechanic')
 			return ESX.UI.Menu.CloseAll()
-		elseif data.current.value == 'put_stock' then
-			OpenPutStocksMenu()
 		elseif data.current.value == 'get_stock' then
 			OpenGetStocksMenu()
 		elseif data.current.value == 'boss_actions' then
@@ -459,53 +455,6 @@ function OpenGetStocksMenu()
 
 					Wait(1000)
 					OpenGetStocksMenu()
-				end
-			end, function(data2, menu2)
-				menu2.close()
-			end)
-		end, function(data, menu)
-			menu.close()
-		end)
-	end)
-end
-
-function OpenPutStocksMenu()
-	ESX.TriggerServerCallback('esx_mechanicjob:getPlayerInventory', function(inventory)
-		local elements = {}
-
-		for i=1, #inventory.items, 1 do
-			local item = inventory.items[i]
-
-			if item.count > 0 then
-				table.insert(elements, {
-					label = item.label .. ' x' .. item.count,
-					type  = 'item_standard',
-					value = item.name
-				})
-			end
-		end
-
-		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'stocks_menu', {
-			title    = _U('inventory'),
-			align    = 'top-left',
-			elements = elements
-		}, function(data, menu)
-			local itemName = data.current.value
-
-			ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'stocks_menu_put_item_count', {
-				title = _U('quantity')
-			}, function(data2, menu2)
-				local count = tonumber(data2.value)
-
-				if count == nil then
-					ESX.ShowNotification(_U('invalid_quantity'))
-				else
-					menu2.close()
-					menu.close()
-					TriggerServerEvent('esx_mechanicjob:putStockItems', itemName, count)
-
-					Wait(1000)
-					OpenPutStocksMenu()
 				end
 			end, function(data2, menu2)
 				menu2.close()
