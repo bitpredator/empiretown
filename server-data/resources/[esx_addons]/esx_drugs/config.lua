511f9a10c1af671dfe5c571ff2e0ee8366a573fe
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-10-12 11:44:41 +0200

    refactor:(the various collection, processing and sale points are hidden)

--- server-data/resources/[esx_addons]/esx_drugs/config.lua
+++ server-data/resources/[esx_addons]/esx_drugs/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Locale = 'it'
+Config.Locale  = 'it'
 
 Config.Delays = {
 	WeedProcessing = 1000 * 7
@@ -19,10 +19,10 @@ Config.LicensePrices = {
 Config.GiveBlack = true -- give black money? if disabled it'll give regular cash.
 
 Config.CircleZones = {
-	WeedField = {coords = vector3(2220.72, 5582.52, 53.81), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 100.0},
-	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_weedprocessing'), color = 25, sprite = 496},
+	WeedField = {coords = vector3(2220.72, 5582.52, 53.81)}, --name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 100.0},
+	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68)}, -- name = _U('blip_weedprocessing'), color = 25, sprite = 496},
 
-	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = _U('blip_drugdealer'), color = 6, sprite = 378},
+	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66)},-- name = _U('blip_drugdealer'), color = 6, sprite = 378},
 }
 
 Config.Marker = {
