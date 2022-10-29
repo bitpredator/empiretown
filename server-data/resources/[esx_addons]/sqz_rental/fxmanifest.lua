27f3ce0c41f2718a083c5b993b84031c978c9dc5
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-10-29 22:47:23 +0200

    feat:(inserted car and bike rental)

---
+++ server-data/resources/[esx_addons]/sqz_rental/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Squizer#3020'
+description 'Script that allows you to borrow vehicles.'
+version '1.0.0'
+
+client_scripts {
+    'warmenu.lua',
+    'config.lua',
+    'locales/*',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'locales/*',
+    'server.lua'
+}
---
+++ server-data/resources/[esx_addons]/sqz_rental/locales/it.lua
@@ -0,0 +1,4 @@
+Locales[it] = {
+    ['you_cannot_return'] = 'you cannot return this vehicle',
+    ['cannot_return'] = 'Non puoi restituire questo veicolo in quanto non è stato noleggiato',
+}
