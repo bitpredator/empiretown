4d2923cc9421e1f518d82ab87899029f07e1c69f
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-11-15 19:31:01 +0100

    fix(esx_multicharacter) - adding missing translations

--- server-data/resources/[esx]/esx_multicharacter/locales/de.lua
+++ server-data/resources/[esx]/esx_multicharacter/locales/de.lua
@@ -3,6 +3,7 @@ Locales['de'] = {
 	['female'] = "Weiblich",
 	['delete_label'] = "Lösche %s %s?",
 	['select_char'] = "Charakter auswählen",
+	["select_char_description"] = "Select a character to play as.",
 	['create_char'] = "Neuen Charakter erstellen",
 	['char_play'] = "Diesen Charakter auswählen",
 	['char_delete'] = "Diesen Charakter löschen",
--- server-data/resources/[esx]/esx_multicharacter/locales/tr.lua
+++ server-data/resources/[esx]/esx_multicharacter/locales/tr.lua
@@ -3,6 +3,7 @@ Locales['tr'] = {
 	['female'] = "Kadın",
 	['delete_label'] = "Sil %s %s?",
 	['select_char'] = "Karakter Seç",
+	["select_char_description"] = "Select a character to play as.",
 	['create_char'] = "Yeni Karakter Oluştur",
 	['char_play'] = "Bu Karakterle Oyna",
 	['char_delete'] = "Bu Karakteri Sil",
