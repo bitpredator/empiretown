2379ca561e45327e8f72e9b93e1e09e84a073662
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-11-28 20:24:09 +0100

    refactor:(created ped in computer store)

--- server-data/resources/[bpt_addons]/bpt_ped/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_ped/client/main.lua
@@ -1,14 +1,9 @@
--- dustman 
+-- Digital Store
 Citizen.CreateThread(function()
 
-  RequestModel(GetHashKey("a_m_m_farmer_01"))
-  while not HasModelLoaded(GetHashKey("a_m_m_farmer_01")) do
-    Wait(155)
-  end
-
-  local ped = CreatePed(4, GetHashKey("a_m_m_farmer_01"), -429.046143, -1729.199951, 18.776611, false, true)
+  local ped = CreatePed(4, GetHashKey("s_f_y_clubbar_01"), 383.762634, -826.681335, 28.300000, false, true)
   FreezeEntityPosition(ped, true)
+  SetEntityHeading(ped, 260.0 )
   SetEntityInvincible(ped, true)
-  SetBlockingOfNonTemporaryEvents(ped, true)
-      
+  SetBlockingOfNonTemporaryEvents(ped, true)  
 end)
