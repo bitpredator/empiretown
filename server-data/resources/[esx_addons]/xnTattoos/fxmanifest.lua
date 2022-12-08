565166abf24d0e1fdfd8c47649abee3fa7982ab5
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-12-08 15:38:11 +0100

    feat:( new code for tattoos)

---
+++ server-data/resources/[esx_addons]/xnTattoos/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+version '0.0.3'
+
+client_scripts {
+	'client/jaymenu.lua',
+	'config.lua',
+	'@es_extended/locale.lua',
+	'client/client.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'server/server.lua'
+}
+
+file 'AllTattoos.json'
---
+++ server-data/resources/[esx_addons]/xnTattoos/server/server.lua
@@ -0,0 +1,50 @@
+ESX = nil
+ESX = exports["es_extended"]:getSharedObject()
+
+ESX.RegisterServerCallback('SmallTattoos:GetPlayerTattoos', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer then
+		MySQL.Async.fetchAll('SELECT tattoos FROM users WHERE identifier = @identifier', {
+			['@identifier'] = xPlayer.identifier
+		}, function(result)
+			if result[1].tattoos then
+				cb(json.decode(result[1].tattoos))
+			else
+				cb()
+			end
+		end)
+	else
+		cb()
+	end
+end)
+
+ESX.RegisterServerCallback('SmallTattoos:PurchaseTattoo', function(source, cb, tattooList, price, tattoo, tattooName)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.getMoney() >= price then
+		xPlayer.removeMoney(price)
+		table.insert(tattooList, tattoo)
+
+		MySQL.Async.execute('UPDATE users SET tattoos = @tattoos WHERE identifier = @identifier', {
+			['@tattoos'] = json.encode(tattooList),
+			['@identifier'] = xPlayer.identifier
+		})
+
+		TriggerClientEvent('esx:showNotification', source, "You have bought the ~y~" .. tattooName .. "~s~ tattoo for ~g~$" .. price)
+		cb(true)
+	else
+		TriggerClientEvent('esx:showNotification', source, "You do not have enough money for this tattoo")
+		cb(false)
+	end
+end)
+
+RegisterServerEvent('SmallTattoos:RemoveTattoo')
+AddEventHandler('SmallTattoos:RemoveTattoo', function (tattooList)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	MySQL.Async.execute('UPDATE users SET tattoos = @tattoos WHERE identifier = @identifier', {
+		['@tattoos'] = json.encode(tattooList),
+		['@identifier'] = xPlayer.identifier
+	})
+end)
