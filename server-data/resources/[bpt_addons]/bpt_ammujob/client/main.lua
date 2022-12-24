54a13317d5835a26040c5e51b23413546b3a6f99
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-12-24 22:49:41 +0100

    fix:(variable)

--- server-data/resources/[bpt_addons]/bpt_ammujob/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_ammujob/client/main.lua
@@ -1,6 +1,6 @@
-local HasAlreadyEnteredMarker,
+local HasAlreadyEnteredMarker, CurrentAction,
     CurrentActionData = false, false, false, false, false, {}
-local LastZone, CurrentAction, CurrentActionMsg
+local LastZone, CurrentActionMsg
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
