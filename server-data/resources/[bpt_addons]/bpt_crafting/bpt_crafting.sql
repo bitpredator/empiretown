b178055113b73c30ec5167edd6a17c617621645f
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-01-05 11:57:09 +0100

    fix:(bpt_crafting item missing in SQL file)

--- server-data/resources/[bpt_addons]/bpt_crafting/bpt_crafting.sql
+++ server-data/resources/[bpt_addons]/bpt_crafting/bpt_crafting.sql
@@ -9,3 +9,38 @@ CREATE TABLE `bpt_items` (
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
+
+INSERT INTO `bpt_items` (`name`, `label`) VALUES
+	('bandage', 'benda'),
+	('cottonforbandages', 'cotone per bende'),
+	('cotton', 'cotone'),
+	('ironsheet', 'lamiera di ferro'),
+	('garbage', 'rifiuti'),
+	('WEAPON_APPISTOL', 'pistola AP'),
+	('iron', 'Ferro'),
+	('hammer', 'martello'),
+	('legnatagliata', 'asse di legno'),
+	('fixkit', 'kit di riparazione'),
+	('almondmilk', 'latte di mandorla'),
+	('ice', 'ghiaccio'),
+	('water', 'acqua'),
+	('almonds', 'mandorle'),
+	('chips', 'patatine fritte'),
+	('slicedchips', 'patate affettate'),
+	('potato', 'patate'),
+	('trash_can', 'lattina usata'),
+	('recycled_paper', 'carta riciclata'),
+	('paper', 'carta'),
+	('newspaper', 'giornale rovinato'),
+	('trash_burgershot', 'scatola di burgershot usata'),
+	('cigarette_paper', 'cartina'),
+	('cigarrette_opium', 'sigaretta con oppio'),
+	('opium', 'oppio'),
+	('copper', 'Rame'),
+	('gold', 'oro'),
+	('gunpowder', 'polvere da sparo'),
+	('ammo-sniper', '7.62 NATO'),
+	('grain', 'grano'),
+	('flour', 'farina'),
+	('bread', 'panino vuoto')
+;
