776c8eb216fc9de1efcd14fbf14ff8fd6b2342c6
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-01-06 14:32:26 +0100

    refactor:(bpt_importjob removed non-essential lines of code)

--- server-data/resources/[bpt_addons]/bpt_importjob/locales/it.lua
+++ server-data/resources/[bpt_addons]/bpt_importjob/locales/it.lua
@@ -4,7 +4,6 @@ Locales['it'] = {
   ['cloakroom_prompt'] = 'premi [E] per accedere al guardaroba.',
   ['wear_citizen'] = 'abiti civili',
   ['wear_work'] = 'abiti da lavoro',
-
   -- garage
   ['spawner_prompt'] = 'premi [E] per accedere al Garage.',
   ["vehicle_spawned"] = "Veicolo ritirato con successo!",
@@ -12,26 +11,15 @@ Locales['it'] = {
   ['spawn_veh'] = 'spawn veicolo',
   ['spawnpoint_blocked'] = 'lo spawnpoint è bloccato !',
   ['only_import'] = 'puoi solo memorizzare import.',
-
   ['taking_service'] = 'prendi servizio: import',
   ['full_service'] = 'servizio completo: ',
   ['amount_invalid'] = 'importo non valido',
   ['press_to_open'] = 'premi [E] per accedere al menu',
   ['billing'] = 'fattura',
-  ['billing_sent'] = 'il conto è stato registrato!',
+  ['billing_sent'] = 'la fattura è stata registrata!',
   ['invoice_amount'] = 'importo della fattura',
   ['no_players_near'] = 'nessun giocatore nelle vicinanze',
-  ['deposit_stock'] = 'deposita',
-  ['take_stock'] = 'ritira',
   ['boss_actions'] = 'Azioni del boss',
-  ['quantity'] = 'quantità',
-  ['quantity_invalid'] = 'quantità non valida!',
-  ['inventory'] = 'inventario',
-  ['have_withdrawn'] = 'hai prelevato x%s %s',
-  ['have_deposited'] = 'hai depositato x%s %s',
-  ['player_cannot_hold'] = 'non hai ~r~abbastanza spazio nel tuo inventario!',
   ['blip_import'] = 'Import/Export',
-  ['phone_import'] = 'Import/Export.',
-  ['import'] = 'import',
-  ['import_stock'] = 'import Stock'
+  ['import'] = 'import'
 }
