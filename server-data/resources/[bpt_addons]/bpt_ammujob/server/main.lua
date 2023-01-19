da75a9e6da3caf64323c4bbbd2b3894ed33b9b26
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-01-19 23:50:08 +0100

    fix:(bpt_ammujob removed unnecessary event)

--- server-data/resources/[bpt_addons]/bpt_ammujob/server/main.lua
+++ server-data/resources/[bpt_addons]/bpt_ammujob/server/main.lua
@@ -8,39 +8,6 @@ TriggerEvent('esx_society:registerSociety', 'ammu', 'Ammu', 'society_ammu', 'soc
     type = 'public'
 })
 
-RegisterNetEvent('bpt_ammujob:success')
-AddEventHandler('bpt_ammujob:success', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local timeNow = os.clock()
-
-    if xPlayer.job.name == 'ammu' then
-        if not lastPlayerSuccess[source] or timeNow - lastPlayerSuccess[source] > 5 then
-            lastPlayerSuccess[source] = timeNow
-
-            if xPlayer.job.grade >= 3 then
-                total = total * 2
-            end
-
-            TriggerEvent('esx_addonaccount:getSharedAccount', 'society_ammu', function(account)
-                if account then
-                    local playerMoney = ESX.Math.Round(total / 100 * 30)
-                    local societyMoney = ESX.Math.Round(total / 100 * 70)
-
-                    xPlayer.addMoney(playerMoney, "Ammu Fair")
-                    account.addMoney(societyMoney)
-
-                    xPlayer.showNotification(_U('comp_earned', societyMoney, playerMoney))
-                else
-                    xPlayer.addMoney(total, "Ammu Fair")
-                    xPlayer.showNotification(_U('have_earned', total))
-                end
-            end)
-        end
-    else
-        print(('[^3WARNING^7] Player ^5%s^7 attempted to ^5bpt_ammujob:success^7 (cheating)'):format(source))
-    end
-end)
-
 ESX.RegisterServerCallback("bpt_ammujob:SpawnVehicle", function(source, cb, model , props)
     local xPlayer = ESX.GetPlayerFromId(source)
 
