3e8d38586791c2c86aee635d28f237617392cede
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-02-03 22:57:57 +0100

    fix:(esx_uteknark2 replaced _resource.lua in fxmanifest)

--- server-data/resources/[esx_addons]/esx_uteknark2/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_uteknark2/fxmanifest.lua
@@ -1,7 +1,7 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
 description 'ESX UteKnark by DemmyDemon'
-
-dependencies {'es_extended','oxmysql'}
+version '0.0.4'
 
 shared_scripts {
     '@es_extended/locale.lua',
@@ -18,4 +18,9 @@ client_scripts {
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'sv_uteknark.lua',
+}
+
+dependencies {
+    'es_extended',
+    'oxmysql'
 }
