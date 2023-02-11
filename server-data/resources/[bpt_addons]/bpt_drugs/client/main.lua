389a78ae064a810e9b3c24e55f2c3f336263d88a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-02-11 23:47:54 +0100

    feat(bpt_drugs)
    new code to replace esx_uteknark2

---
+++ server-data/resources/[bpt_addons]/bpt_drugs/client/main.lua
@@ -0,0 +1,37 @@
+local menuOpen = false
+local cfgMarker = Config.Marker;
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		if menuOpen then
+			ESX.CloseContext()
+		end
+	end
+end)
+
+function CreateBlipCircle(coords, text, radius, color, sprite)
+	local blip = AddBlipForRadius(coords, radius)
+
+	SetBlipHighDetail(blip, true)
+	SetBlipColour(blip, 1)
+	SetBlipAlpha (blip, 128)
+
+	-- create a blip in the middle
+	blip = AddBlipForCoord(coords)
+
+	SetBlipHighDetail(blip, true)
+	SetBlipSprite (blip, sprite)
+	SetBlipScale  (blip, 1.0)
+	SetBlipColour (blip, color)
+	SetBlipAsShortRange(blip, true)
+
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentSubstringPlayerName(text)
+	EndTextCommandSetBlipName(blip)
+end
+
+CreateThread(function()
+	for k,zone in pairs(Config.CircleZones) do
+		CreateBlipCircle(zone.coords, zone.name, zone.radius, zone.color, zone.sprite)
+	end
+end)
