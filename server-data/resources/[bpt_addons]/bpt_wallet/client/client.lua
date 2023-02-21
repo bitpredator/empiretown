893520465b4e974d4925c5058e07636908a46dcb
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-02-21 23:39:17 +0100

    feat(bpt_wallet)
    new code to have the wallet as an item

---
+++ server-data/resources/[bpt_addons]/bpt_wallet/client/client.lua
@@ -0,0 +1,59 @@
+local bagEquipped, bagObj
+local ox_inventory = exports.ox_inventory
+local ped = cache.ped
+local justConnect = true
+
+local function PutOnBag()
+    local x, y, z = table.unpack(GetOffsetFromEntityInWorldCoords(ped,0.0,3.0,0.5))
+    lib.requestModel(hash, 100)
+    bagObj = CreateObjectNoOffset(hash, x, y, z, true, false)
+    AttachEntityToEntity(bagObj, ped, GetPedBoneIndex(ped, 24818), 0.07, -0.11, -0.05, 0.0, 90.0, 175.0, true, true, false, true, 1, true)
+    bagEquipped = true
+end
+
+local function RemoveBag()
+    if DoesEntityExist(bagObj) then
+        DeleteObject(bagObj)
+    end
+    SetModelAsNoLongerNeeded(hash)
+    bagObj = nil
+    bagEquipped = nil
+end
+
+AddEventHandler('ox_inventory:updateInventory', function(changes)
+    if justConnect then
+        Wait(4500)
+        justConnect = nil
+    end
+    for k, v in pairs(changes) do
+        if type(v) == 'table' then
+            local count = ox_inventory:Search('count', Config.WalletItem)
+	        if count > 0 and (not bagEquipped or not bagObj) then
+                PutOnBag()
+            elseif count < 1 and bagEquipped then
+                RemoveBag()
+            end
+        end
+        if type(v) == 'boolean' then
+            local count = ox_inventory:Search('count', Config.WalletItem)
+            if count < 1 and bagEquipped then
+                RemoveBag()
+            end
+        end
+    end
+end)
+
+lib.onCache('ped', function(value)
+    ped = value
+end)
+
+exports('openWallet', function(data, slot)
+    if not slot?.metadata?.identifier then
+        local identifier = lib.callback.await('bpt_wallet:getNewIdentifier', 100, data.slot)
+        ox_inventory:openInventory('stash', 'wal_'..identifier)
+    else
+        TriggerServerEvent('bpt_wallet:openWallet', slot.metadata.identifier)
+        ox_inventory:openInventory('stash', 'wal_'..slot.metadata.identifier)
+    end
+end)
+
---
+++ server-data/resources/[bpt_addons]/bpt_wallet/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.WalletItem = 'wallet' -- Item name of wallet
+Config.WalletStorage = {
+    slots = 8, -- Slots of wallet storage
+    weight = 10000 -- Total weight for wallet
+}
