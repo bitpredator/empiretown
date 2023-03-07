af41be4476fdf216d4f591134feab70a1fb13bdc
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-03-08 00:00:57 +0100

    fix(esx_garage\fxmanifest.lua): defined in fxmanifest.lua

--- server-data/resources/[esx_addons]/esx_garage/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_garage/fxmanifest.lua
@@ -3,15 +3,31 @@ game 'gta5'
 
 author 'bitpredator'
 description 'BPT Garage System compatible with esx'
-version '0.0.3'
+version '0.0.4'
 lua54 'yes'
 
 shared_script '@es_extended/imports.lua'
 
-server_scripts {'@es_extended/locale.lua', 'locales/*.lua', '@oxmysql/lib/MySQL.lua', 'config.lua', 'server/main.lua'}
+server_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua'
+}
 
-client_scripts {'@es_extended/locale.lua', 'locales/*.lua', 'config.lua', 'client/main.lua'}
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua',
+    'client/main.lua'
+}
 
 ui_page 'nui/ui.html'
 
-files {'nui/ui.html', 'nui/js/*.js', 'nui/css/*.css', 'nui/img/*.png', 'nui/roboto.ttf'}
+files {
+ 'nui/ui.html',
+ 'nui/js/*.js',
+ 'nui/css/*.css',
+ 'nui/roboto.ttf'
+}
