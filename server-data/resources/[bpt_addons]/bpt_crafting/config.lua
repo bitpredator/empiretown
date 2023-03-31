2d5a77aec7648e158f1985956c7ed7f125290866
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-03-31 20:27:59 +0200

    fix(bpt_crafting): whitespace fix

--- server-data/resources/[bpt_addons]/bpt_crafting/config.lua
+++ server-data/resources/[bpt_addons]/bpt_crafting/config.lua
@@ -1,79 +1,75 @@
 Config = {
-
 UseLimitSystem = false, -- Enable if your esx uses limit system
-
 CraftingStopWithDistance = false, -- Crafting will stop when not near workbench
-
 ExperiancePerCraft = 5, -- The amount of experiance added per craft (100 Experiance is 1 level)
-
 HideWhenCantCraft = false, -- Instead of lowering the opacity it hides the item that is not craftable due to low level or wrong job
 
 Categories = {
 	['weapons'] = {
-		Label = 'ARMI',
-		Image = 'WEAPON_APPISTOL',
-		Jobs = {'ammu'}
+	 Label = 'ARMI',
+	 Image = 'WEAPON_APPISTOL',
+	 Jobs = {'ammu'}
 	},
 	['medical'] = {
-		Label = 'Ospedale',
-		Image = 'bandage',
-		Jobs = {'ambulance'}
+	 Label = 'Ospedale',
+	 Image = 'bandage',
+	 Jobs = {'ambulance'}
 	},
 	['import'] = {
-		Label = 'Import',
-		Image = 'Import',
-		Jobs = {'import'}
+	 Label = 'Import',
+	 Image = 'Import',
+	 Jobs = {'import'}
 	},
 	['mechanic'] = {
-		Label = 'Meccanico',
-		Image = 'Mechanic',
-		Jobs = {'mechanic'}
+	 Label = 'Meccanico',
+	 Image = 'Mechanic',
+	 Jobs = {'mechanic'}
 	},
 	['ammu'] = {
-		Label = 'Armeria',
-		Image = 'ammu',
-		Jobs = {'ammu'}
+	 Label = 'Armeria',
+	 Image = 'ammu',
+	 Jobs = {'ammu'}
 	},
 	['unicorn'] = {
-		Label = 'Unicorn',
-		Image = 'unicorn',
-		Jobs = {'unicorn'}
+	 Label = 'Unicorn',
+	 Image = 'unicorn',
+	 Jobs = {'unicorn'}
 	},
 	['dustman'] = {
-		Label = 'Dustman',
-		Image = 'dustman',
-		Jobs = {'dustman'}
+	 Label = 'Dustman',
+	 Image = 'dustman',
+	 Jobs = {'dustman'}
 	},
 	['ballas'] = {
-		Label = 'Ballas',
-		Image = 'ballas',
-		Jobs = {'ballas'}
+	 Label = 'Ballas',
+	 Image = 'ballas',
+	 Jobs = {'ballas'}
 	},
 	['baker'] = {
-		Label = 'Baker',
-		Image = 'baker',
-		Jobs = {'baker'}
+	 Label = 'Baker',
+	 Image = 'baker',
+	 Jobs = {'baker'}
 	}
 },
 
 PermanentItems = { -- Items that dont get removed when crafting
-	['wrench'] = true
+ ['wrench'] = true
 },
 
 Recipes = { -- Enter Item name and then the speed value! The higher the value the more torque
 
 ['bandage'] = {
-	Level = 0, -- From what level this item will be craftable
-	Category = 'medical', -- The category item will be put in
-	isGun = false, -- Specify if this is a gun so it will be added to the loadout
-	Jobs = {'ambulance'}, -- What jobs can craft this item, leaving {} allows any job
-	JobGrades = {}, -- What job grades can craft this item, leaving {} allows any grade
-	Amount = 2, -- The amount that will be crafted
-	SuccessRate = 100, -- 100% you will recieve the item
-	requireBlueprint = false, -- Requires a blueprint whitch you need to add in the database yourself TEMPLATE: itemname_blueprint EXAMPLE: bandage_blueprint
-	Time = 10, -- Time in seconds it takes to craft this item
-	Ingredients = { -- Ingredients needed to craft this item
-		['cottonforbandages'] = 2 -- item name and count, adding items that dont exist in database will crash the script
+ Level = 0, -- From what level this item will be craftable
+ Category = 'medical', -- The category item will be put in
+ isGun = false, -- Specify if this is a gun so it will be added to the loadout
+ Jobs = {'ambulance'}, -- What jobs can craft this item, leaving {} allows any job
+ JobGrades = {}, -- What job grades can craft this item, leaving {} allows any grade
+ Amount = 2, -- The amount that will be crafted
+ SuccessRate = 100, -- 100% you will recieve the item
+ requireBlueprint = false, -- Requires a blueprint whitch you need to add in the database yourself TEMPLATE: itemname_blueprint EXAMPLE: bandage_blueprint
+ Time = 10, -- Time in seconds it takes to craft this item
+ Ingredients = { -- Ingredients needed to craft this item
+	 ['cottonforbandages'] = 2 -- item name and count, adding items that dont exist in database will crash the script
 	}
 }, 
 
@@ -302,74 +298,70 @@ Recipes = { -- Enter Item name and then the speed value! The higher the value th
 },
 
 ['bread'] = {
-	Level = 0,
-	Category = 'baker', 
-	isGun = false, 
-	Jobs = {'baker'}, 
-	JobGrades = {}, 
-	Amount = 5, 
-	SuccessRate = 100, 
-	requireBlueprint = false, 
-	Time = 200, 
+ Level = 0,
+ Category = 'baker', 
+ isGun = false, 
+ Jobs = {'baker'}, 
+ JobGrades = {}, 
+ Amount = 5, 
+ SuccessRate = 100, 
+ requireBlueprint = false, 
+ Time = 200, 
 	Ingredients = { 
 		['flour'] = 1
 	}
 },
 
 ['flour'] = {
-	Level = 0,
-	Category = 'baker', 
-	isGun = false, 
-	Jobs = {'baker'}, 
-	JobGrades = {}, 
-	Amount = 1, 
-	SuccessRate = 100, 
-	requireBlueprint = false, 
-	Time = 120, 
+ Level = 0,
+ Category = 'baker', 
+ isGun = false, 
+ Jobs = {'baker'}, 
+ JobGrades = {}, 
+ Amount = 1, 
+ SuccessRate = 100, 
+ requireBlueprint = false, 
+ Time = 120, 
 	Ingredients = { 
 		['grain'] = 10
 	}
 },
 
 ['bread_deer'] = {
-	Level = 0,
-	Category = 'unicorn', 
-	isGun = false, 
-	Jobs = {'unicorn'}, 
-	JobGrades = {}, 
-	Amount = 3, 
-	SuccessRate = 100, 
-	requireBlueprint = false, 
-	Time = 120, 
+ Level = 0,
+ Category = 'unicorn', 
+ isGun = false, 
+ Jobs = {'unicorn'}, 
+ JobGrades = {}, 
+ Amount = 3, 
+ SuccessRate = 100, 
+ requireBlueprint = false, 
+ Time = 120, 
 	Ingredients = { 
-		['deer_meat'] = 1,
-		['bread'] = 3
+	 ['deer_meat'] = 1,
+	 ['bread'] = 3
 	}
 },},
 
 Workbenches = { -- Every workbench location, leave {} for jobs if you want everybody to access
-
-	{coords = vector3(1020.936279, -2404.628662, 30.122314), jobs = {'import'}, blip = false, recipes = {'cottonforbandages','iron','hammer'}, radius = 1.0 },
-	{coords = vector3(311.314301, -565.213196, 43.282104), jobs = {'ambulance'}, blip = false, recipes = {'bandage'}, radius = 1.0 },
-	{coords = vector3(-323.551636, -129.626373, 39.002197), jobs = {'mechanic'}, blip = false, recipes = {'fixkit'}, radius = 1.0 },
-	{coords = vector3(809.090088, -2172.923096, 29.616821), jobs = {'ammu'}, blip = false, recipes = {'WEAPON_APPISTOL','ammo-sniper'}, radius = 1.0 },
-	{coords = vector3(129.217590, -1283.802246, 29.263062), jobs = {'unicorn'}, blip = false, recipes = {'almondmilk','slicedchips','bread_deer'}, radius = 1.0 },
-    {coords = vector3(-416.993408, -1683.468140, 19.018311), jobs = {'dustman'}, blip = false, recipes = {'ironsheet','recycled_paper','paper'}, radius = 1.0 },
-	{coords = vector3(83.156044, -1960.259277, 18.041016), jobs = {'ballas'}, blip = false, recipes = {'cigarette_paper'}, radius = 1.0 },
-	{coords = vector3(2342.202148, 3144.817627, 48.202148), jobs = {'baker'}, blip = false, recipes = {'flour', 'bread'}, radius = 1.0 },
+ {coords = vector3(1020.936279, -2404.628662, 30.122314), jobs = {'import'}, blip = false, recipes = {'cottonforbandages','iron','hammer'}, radius = 1.0 },
+ {coords = vector3(311.314301, -565.213196, 43.282104), jobs = {'ambulance'}, blip = false, recipes = {'bandage'}, radius = 1.0 },
+ {coords = vector3(-323.551636, -129.626373, 39.002197), jobs = {'mechanic'}, blip = false, recipes = {'fixkit'}, radius = 1.0 },
+ {coords = vector3(809.090088, -2172.923096, 29.616821), jobs = {'ammu'}, blip = false, recipes = {'WEAPON_APPISTOL','ammo-sniper'}, radius = 1.0 },
+ {coords = vector3(129.217590, -1283.802246, 29.263062), jobs = {'unicorn'}, blip = false, recipes = {'almondmilk','slicedchips','bread_deer'}, radius = 1.0 },
+ {coords = vector3(-416.993408, -1683.468140, 19.018311), jobs = {'dustman'}, blip = false, recipes = {'ironsheet','recycled_paper','paper'}, radius = 1.0 },
+ {coords = vector3(83.156044, -1960.259277, 18.041016), jobs = {'ballas'}, blip = false, recipes = {'cigarette_paper'}, radius = 1.0 },
+ {coords = vector3(2342.202148, 3144.817627, 48.202148), jobs = {'baker'}, blip = false, recipes = {'flour', 'bread'}, radius = 1.0 },
 },
  
-
 Text = {
-
-    ['not_enough_ingredients'] = 'Non hai abbastanza ingredienti',
-    ['you_cant_hold_item'] = 'Non puoi creare il prodotto',
-    ['item_crafted'] = 'Articolo realizzato!',
-    ['wrong_job'] = 'Non puoi aprire questo banco di lavoro',
-    ['workbench_hologram'] = '[~b~E~w~] banco da lavoro',
-    ['inv_limit_exceed'] = 'Limite inventario superato! Rimuovi qualcosa prima di perdere altra merce',
-    ['crafting_failed'] = 'Non sei riuscito a creare l\'oggetto!'
-
+ ['not_enough_ingredients'] = 'Non hai abbastanza ingredienti',
+ ['you_cant_hold_item'] = 'Non puoi creare il prodotto',
+ ['item_crafted'] = 'Articolo realizzato!',
+ ['wrong_job'] = 'Non puoi aprire questo banco di lavoro',
+ ['workbench_hologram'] = '[~b~E~w~] banco da lavoro',
+ ['inv_limit_exceed'] = 'Limite inventario superato! Rimuovi qualcosa prima di perdere altra merce',
+ ['crafting_failed'] = 'Non sei riuscito a creare l\'oggetto!'
 }}
 
 
