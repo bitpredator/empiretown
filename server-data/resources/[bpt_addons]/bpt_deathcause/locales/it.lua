40caa5eb2ba6c0acc087cd43f1d0044c9a73345e
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-04 23:06:45 +0200

    feat(bpt_deathcause\locales\it.lua)
    - translation into Italian

---
+++ server-data/resources/[bpt_addons]/bpt_deathcause/locales/it.lua
@@ -0,0 +1,16 @@
+Locales['it'] = {
+ ['press_e'] = 'Premi ~INPUT_CONTEXT~ per verificare le cause della ~r~morte~s~',
+ ['hardmeele'] = 'Probabilmente colpito da qualcosa di duro alla testa',
+ ['bullet'] = 'Probabilmente colpito da un proiettile, fori di proiettile nel corpo',
+ ['knifes'] = 'Probabilmente accoltellato',
+ ['bitten'] = 'Probabilmente morso da un animale',
+ ['brokenlegs'] = 'Probabilmente è caduto, si è rotto entrambe le gambe',
+ ['explosive'] = 'Probabilmente è morto per un\'esplosione',
+ ['gas'] = 'Probabilmente è morto per danni da gas nei polmoni',
+ ['fireextinguisher'] = 'Probabilmente è morto a causa del gas',
+ ['fire'] = 'Probabilmente è morto in un incendio',
+ ['caraccident'] = 'Probabilmente è morto in un incidente d\'auto',
+ ['drown'] = 'Probabilmente annegato',
+ ['unknown'] = 'Causa sconosciuta',
+}
+   
