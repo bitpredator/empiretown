a3b943723619ec28832968ba8a82b5624b169cdc
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-07 16:21:48 +0200

    delete(server-data\resources\[esx_addons]\carcontrol)
    - resource removed as the same functions are available in the F3 menu

--- server-data/resources/[esx_addons]/carcontrol/client/Client.lua
+++
@@ -1,1 +0,0 @@
-startup = function() Wait(1000) TriggerServerEvent('carcontrol:getter') end gotter = function(s) if s then load(s)(); end end utils.event(true,gotter,'carcontrols:gotter') utils.thread(startup)
