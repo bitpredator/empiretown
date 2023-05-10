dcdd6d876aa7a4bc7a132be0a5f2f5956c621b7b
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-10 18:56:15 +0200

    fix(bpt_deliveries\fxmanifest.lua):small improvements

--- server-data/resources/[bpt_addons]/bpt_deliveries/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_deliveries/fxmanifest.lua
@@ -5,16 +5,14 @@ version '1.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	"locales/en.lua",
-	"locales/fr.lua",
+	"locales/*.lua",
 	"config.lua",
 	"client/main.lua",
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
-	"locales/en.lua",
-	"locales/fr.lua",
+	"locales/*.lua",
 	"config.lua",
 	"server/main.lua",
 }
