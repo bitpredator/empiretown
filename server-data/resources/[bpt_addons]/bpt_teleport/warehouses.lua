b5ea1b2d704ced9a6f1f9560fe4aa950384b380e
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-18 10:17:34 +0200

    refactor(bpt_teleport\): fixed reported issues fivem lua action

--- server-data/resources/[bpt_addons]/bpt_teleport/warehouses.lua
+++ server-data/resources/[bpt_addons]/bpt_teleport/warehouses.lua
@@ -13,18 +13,19 @@ CreateThread(function()
             if IsControlPressed(0, 51) then
                 DoScreenFadeOut(1000)
                 Wait(1500)
-                SetEntityCoords(PlayerPedId(), 1022.123108, -2398.377930, 30.122314) 
+                SetEntityCoords(PlayerPedId(), 1022.123108, -2398.377930, 30.122314)
                 Wait(1000)
                 DoScreenFadeIn(1000)
             end
         end
+
         -- import exit
         if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, 1022.123108, -2398.377930, 30.122314, true) <= 3.0 then
             ESX.ShowHelpNotification(_U('press_to_exit'))
             if IsControlPressed(0, 51) then
                 DoScreenFadeOut(1000)
                 Wait(1500)
-                SetEntityCoords(PlayerPedId(), 1025.459351, -2398.865967, 29.903320, 239.736) 
+                SetEntityCoords(PlayerPedId(), 1025.459351, -2398.865967, 29.903320, 239.736)
                 Wait(1000)
                 DoScreenFadeIn(1000)
             end
@@ -42,7 +43,7 @@ CreateThread(function()
             if IsControlPressed(0, 51) then
                 DoScreenFadeOut(1000)
                 Wait(1500)
-                SetEntityCoords(PlayerPedId(), 126.742, -1278.386, 28.569) 
+                SetEntityCoords(PlayerPedId(), 126.742, -1278.386, 28.569)
                 Wait(1000)
                 DoScreenFadeIn(1000)
             end
@@ -53,7 +54,7 @@ CreateThread(function()
             if IsControlPressed(0, 51) then
                 DoScreenFadeOut(1000)
                 Wait(1500)
-                SetEntityCoords(PlayerPedId(), 132.527481, -1294.153809, 29.263062) 
+                SetEntityCoords(PlayerPedId(), 132.527481, -1294.153809, 29.263062)
                 Wait(1000)
                 DoScreenFadeIn(1000)
             end
