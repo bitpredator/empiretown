88456959036273998e783c79b184b9ad7b1e46e6
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-28 10:45:04 +0200

    feat: (bpt_dustman\locales\en.lua) new English translation

---
+++ server-data/resources/[bpt_addons]/bpt_dustman/locales/en.lua
@@ -0,0 +1,25 @@
+Locales['en'] = {
+  -- cloakroom
+  ['cloakroom_menu'] = 'wardrobe',
+  ['cloakroom_prompt'] = 'press [E] to access the cloakroom.',
+  ['wear_citizen'] = 'civilian clothes',
+  ['wear_work'] = 'work clothes',
+  -- garage
+  ['spawner_prompt'] = 'press [E] to access the Garage.',
+  ["vehicle_spawned"] = "Vehicle picked up successfully!",
+  ['store_veh'] = 'press [E] to deposit the vehicle',
+  ['spawn_veh'] = 'spawn vehicle',
+  ['spawnpoint_blocked'] = 'the spawnpoint is blocked !',
+  ['only_dustman'] = 'you can only deposit company vehicles.',
+  ['taking_service'] = 'take service: dustman',
+  ['full_service'] = 'complete service: ',
+  ['amount_invalid'] = 'invalid amount',
+  ['press_to_open'] = 'press [E] to access the menu',
+  ['billing'] = 'billing',
+  ['billing_sent'] = 'the account has been registered!',
+  ['invoice_amount'] = 'invoice amount',
+  ['no_players_near'] = 'no players nearby',
+  ['boss_actions'] = 'boss action',
+  ['blip_dustman'] = 'dump',
+  ['dustman'] = 'dustman'
+}
