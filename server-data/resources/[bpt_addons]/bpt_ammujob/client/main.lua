5bea28990c4338fa91b0a8bdd06cdb9039f8e0fd
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-02 21:52:23 +0200

    chore: (bpt_ammujob\client\main.lua)  fix right side of assignment has more values than left side

--- server-data/resources/[bpt_addons]/bpt_ammujob/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_ammujob/client/main.lua
@@ -1,6 +1,6 @@
 local HasAlreadyEnteredMarker
-local CurrentAction, CurrentActionData = false, false, false, false, false, {}
-local LastZone, CurrentActionMsg
+local CurrentAction, CurrentActionMsg, CurrentActionData = nil, '', {}
+local LastZone
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
