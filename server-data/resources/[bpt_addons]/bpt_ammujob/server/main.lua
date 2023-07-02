8f256c63e6b6e215f0bd9fb94b11c0b0c5233f76
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-02 22:11:49 +0200

    chore: (bpt_ammujob\server\main.lua) fix unused argument vehicle

--- server-data/resources/[bpt_addons]/bpt_ammujob/server/main.lua
+++ server-data/resources/[bpt_addons]/bpt_ammujob/server/main.lua
@@ -14,7 +14,7 @@ ESX.RegisterServerCallback("bpt_ammujob:SpawnVehicle", function(source, cb, mode
         return
     end
     local SpawnPoint = vector3(Config.Zones.VehicleSpawnPoint.Pos.x, Config.Zones.VehicleSpawnPoint.Pos.y, Config.Zones.VehicleSpawnPoint.Pos.z)
-    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function(vehicle)
+    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function()
         local vehicle = NetworkGetEntityFromNetworkId()
         while GetVehicleNumberPlateText(vehicle) ~= props.plate do
             Wait(0)
