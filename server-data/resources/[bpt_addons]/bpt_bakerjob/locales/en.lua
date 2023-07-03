3d3a13f8eb6858cbcb92e9a2431433907f3626b9
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-03 17:27:04 +0200

    feat:(bpt_bakerjob\locales\en.lua)  added english translation

---
+++ server-data/resources/[bpt_addons]/bpt_bakerjob/locales/en.lua
@@ -0,0 +1,28 @@
+Locales['en'] = {
+  --cloakroom
+  ['cloakroom_menu'] = 'wardrobe',
+  ['cloakroom_prompt'] = 'press [E] to access the cloakroom.',
+  ['wear_citizen'] = 'civilian clothing',
+  ['wear_work'] = 'work clothes',
+  -- garage
+  ['spawner_prompt'] = 'press [E] to access the garage.',
+  ["vehicle_spawned"] = "vehicle spawned successfully!",
+  ['store_veh'] = 'press [E] to store vehicle',
+  ['spawn_veh'] = 'retire vehicle',
+  ['spawnpoint_blocked'] = 'a vehicle is blocking the spawnpoint!',
+  ['only_baker'] = 'you can only deposit company vehicles.',
+  ['empty_garage'] = 'no vehicles in the garage!',
+  ['taking_service'] = 'taking service: Baker',
+  ['full_service'] = 'full service: ',
+  ['amount_invalid'] = 'invalid amount',
+  ['press_to_open'] = 'press [E] to access the menu',
+  ['billing'] = 'invoice',
+  ['billing_sent'] = 'billing has been posted!',
+  ['invoice_amount'] = 'invoice amount',
+  ['no_players_near'] = 'no players nearby',
+  ['have_earned'] = 'you have earned $%s',
+  ['comp_earned'] = 'your company earned $%s \n - you earned $%s',
+  ['boss_actions'] = 'Boss actions',
+  ['blip_baker'] = 'Baker.',
+  ['baker'] = 'Baker'
+}
