8c0b5d2f6af7159d7019af50dcab2f749c285220
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-12 18:12:22 +0200

    fix:(bpt_dustman\server\main.lua): variable vehicle was previously defined as an argument

--- server-data/resources/[bpt_addons]/bpt_dustman/server/main.lua
+++ server-data/resources/[bpt_addons]/bpt_dustman/server/main.lua
@@ -10,8 +10,8 @@ ESX.RegisterServerCallback("bpt_dustmanjob:SpawnVehicle", function(source, cb, m
         return
     end
     local SpawnPoint = vector3(Config.Zones.VehicleSpawnPoint.Pos.x, Config.Zones.VehicleSpawnPoint.Pos.y, Config.Zones.VehicleSpawnPoint.Pos.z)
-    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function(vehicle)
-        local vehicle = NetworkGetEntityFromNetworkId(vehicle)
+    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function()
+        local vehicle = NetworkGetEntityFromNetworkId()
         while GetVehicleNumberPlateText(vehicle) ~= props.plate do
             Wait(0)
         end
