2ebbb7fada52e3fbea259a1a2b2e9761c1e920f0
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-17 20:48:08 +0200

    delete: removed resources that are no longer needed

--- server-data/resources/[esx]/esx_skin/locales/fi.lua
+++
@@ -1,6 +0,0 @@
-Locales["fi"] = {
-  ["skin_menu"] = "Ulkonäkö",
-  ["use_rotate_view"] = "Paina ~INPUT_FRONTEND_LS~ tai ~INPUT_CHARACTER_WHEEL~ liikutaaksesi kameraa.",
-  ["skin"] = "Muokkaa ulkonäköä",
-  ["saveskin"] = "Tallenna",
-}
--- server-data/resources/[esx]/esx_skin/locales/fr.lua
+++
@@ -1,6 +0,0 @@
-Locales["fr"] = {
-  ["skin_menu"] = "menu de Skin",
-  ["use_rotate_view"] = "utilisez ~INPUT_FRONTEND_LS~ et ~INPUT_CHARACTER_WHEEL~ pour tourner la vue.",
-  ["skin"] = "changer de skin",
-  ["saveskin"] = "sauvegarder skin dans un fichier",
-}
