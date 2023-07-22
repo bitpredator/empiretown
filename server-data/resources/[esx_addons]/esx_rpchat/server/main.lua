cff557eee3317e64e54e29ce49184ea2f60c28cb
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-22 10:45:25 +0200

    fix: accessing undefined variable playerName

--- server-data/resources/[esx_addons]/esx_rpchat/server/main.lua
+++ server-data/resources/[esx_addons]/esx_rpchat/server/main.lua
@@ -2,6 +2,7 @@ AddEventHandler('chatMessage', function(playerId, _, message)
 	if string.sub(message, 1, string.len('/')) ~= '/' then
 		CancelEvent()
 
+        local playerName
 		playerName = GetRealPlayerName(playerId)
 		TriggerClientEvent('chat:addMessage', -1, {args = {_U('ooc_prefix', playerName), message}, color = {128, 128, 128}})
 	end
