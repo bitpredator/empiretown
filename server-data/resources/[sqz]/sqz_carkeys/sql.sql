049d6b1838a6b2d920f3bc5724b487ce784c310e
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-07-23 11:48:42 +0200

    fix: 'peopleWithKeys' cannot have a default value

--- server-data/resources/[sqz]/sqz_carkeys/sql.sql
+++ server-data/resources/[sqz]/sqz_carkeys/sql.sql
@@ -1,6 +1,3 @@
 ALTER TABLE `owned_vehicles`
-	ADD COLUMN `peopleWithKeys` LONGTEXT NULL DEFAULT '[]';
-
-	-- IF you got errors while executing the query above, use the one bellow
-ALTER TABLE `owned_vehicles`
-	ADD COLUMN `peopleWithKeys` LONGTEXT NULL;
+ ADD COLUMN `peopleWithKeys` LONGTEXT NULL
+;
