df7cf97e9c1d80fa5eaaa98b91b988c3d5555928
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-08-22 16:23:35 +0200

    feat: new translations available

---
+++ server-data/resources/[esx_addons]/esx_billing/locales/sr.lua
@@ -0,0 +1,12 @@
+Locales['sr'] = {
+  ['invoices'] = 'Računi',
+  ['invoices_item'] = '$%s',
+  ['received_invoice'] = 'Vi ste ~r~dobili novi račun',
+  ['paid_invoice'] = 'Platili ste račun od ~r~$%s',
+  ['no_invoices'] = 'Nemate računa za plaćanje trenutno',
+  ['received_payment'] = 'Primili ste uplatu u iznosu od ~r~$%s',
+  ['player_not_online'] = 'Osoba nije tu',
+  ['no_money'] = 'Nemate dovoljno novca da platite račun',
+  ['target_no_money'] = 'Osoba ~r~nema dovoljno novca da plati račun!',
+  ['keymap_showbills'] = 'Otvori Račune',
+}
