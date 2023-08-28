700cb3253e27b6fbaf3e319bffd255d0e0d60f64
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-08-28 22:08:11 +0200

    chore:(fix) correct the position of the various blips

--- server-data/resources/[bpt_addons]/bpt_fisherman/config.lua
+++ server-data/resources/[bpt_addons]/bpt_fisherman/config.lua
@@ -11,35 +11,35 @@ Config.AuthorizedVehicles = {
 Config.Zones = {
 
 	VehicleSpawner = {
-		Pos   = {x = 1021.912109, y = -2372.043945, z = 30.526733},
+		Pos   = {x = -351.956055, y = -2790.316406, z = 5.993408},
 		Size  = {x = 0.6, y = 0.6, z = 0.6},
 		Color = {r = 204, g = 204, b = 0},
 		Type  = 36, Rotate = true
 	},
 
 	VehicleSpawnPoint = {
-		Pos     = {x = 1015.740662, y = -2366.624268, z = 30.493042},
+		Pos     = {x = -363.270325, y = -2775.824219, z = 5.993408},
 		Size    = {x = 1.5, y = 1.5, z = 1.0},
 		Type    = -1, Rotate = false,
 		Heading = 225.0
 	},
 
 	VehicleDeleter = {
-		Pos   = {x = 1015.740662, y = -2366.624268, z = 29.493042},
+		Pos   = {x = -348.118683, y = -2760.224121, z = 5.027100},
 		Size  = {x = 3.0, y = 3.0, z = 0.25},
 		Color = {r = 255, g = 0, b = 0},
 		Type  = 1, Rotate = false
 	},
 
 	FishermanActions = {
-		Pos   = {x = 1006.799988, y = -2404.127441, z = 30.122314},
+		Pos   = {x = -327.547241, y = -2769.125244, z = 5.201416},
 		Size  = {x = 0.6, y = 0.6, z = 0.6},
 		Color = {r = 204, g = 204, b = 0},
 		Type  = 20, Rotate = true
 	},
 
 	Cloakroom = {
-		Pos     = {x = 1011.837341, y = -2390.004395, z = 30.122314},
+		Pos     = {x = -323.604401, y = -2784.065918, z = 5.201416},
 		Size    = {x = 0.6, y = 0.6, z = 0.6},
 		Color   = {r = 204, g = 204, b = 0},
 		Type    = 21, Rotate = true
