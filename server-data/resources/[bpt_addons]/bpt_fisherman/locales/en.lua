929532d3a13a20f68e71e506652443bdd6a82726
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-08-29 23:03:04 +0200

    feat:( translation into English)

---
+++ server-data/resources/[bpt_addons]/bpt_fisherman/locales/en.lua
@@ -0,0 +1,23 @@
+Locales['en'] = {
+  -- cloakroom
+  ['cloakroom_menu'] = 'cloakroom',
+  ['cloakroom_prompt'] = 'press [E] to access the cloakroom.',
+  ['wear_citizen'] = 'ivilian clothes',
+  ['wear_work'] = 'work clothes',
+  -- garage
+  ['spawner_prompt'] = 'press [E] to access the Garage.',
+  ["vehicle_spawned"] = "Vehicle picked up successfully!",
+  ['store_veh'] = 'press [E] to deposit the vehicle',
+  ['spawn_veh'] = 'vehicle spawns',
+  ['spawnpoint_blocked'] = 'the spawnpoint is blocked!',
+  ['only_fisherman'] = 'you can only deposit company vehicles.',
+  ['amount_invalid'] = 'invalid amount',
+  ['press_to_open'] = 'press [E] to access the menu',
+  ['billing'] = 'biling',
+  ['billing_sent'] = 'the invoice has been posted!',
+  ['invoice_amount'] = 'invoice amount',
+  ['no_players_near'] = 'no players nearby',
+  ['boss_actions'] = 'Boss actions',
+  ['blip_fisherman'] = 'Fisherman',
+  ['fisherman'] = 'fisherman'
+}
