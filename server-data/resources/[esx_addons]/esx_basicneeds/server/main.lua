de30900380baf1f452fdb88b73e6c1bae5483109
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-09-20 23:42:12 +0200

    chore:  removal of superfluous code
    - Remove unnecessary code as it is handled in ox_inventory

--- server-data/resources/[esx_addons]/esx_basicneeds/server/main.lua
+++ server-data/resources/[esx_addons]/esx_basicneeds/server/main.lua
@@ -1,25 +1,3 @@
-CreateThread(function()
-	for k,v in pairs(Config.Items) do
-		ESX.RegisterUsableItem(k, function(source)
-			local xPlayer = ESX.GetPlayerFromId(source)
-			if v.remove then
-				xPlayer.removeInventoryItem(k,1)
-			end
-			if v.type == "food" then
-				TriggerClientEvent("esx_status:add", source, "hunger", v.status)
-				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop, v.anim)
-				xPlayer.showNotification(_U('used_food', ESX.GetItemLabel(k)))
-			elseif v.type == "drink" then
-				TriggerClientEvent("esx_status:add", source, "thirst", v.status)
-				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop, v.anim)
-				xPlayer.showNotification(_U('used_drink', ESX.GetItemLabel(k)))
-			else
-				print(string.format('^1[ERROR]^0 %s has no correct type defined.', k))
-			end
-		end)
-	end 
-end)
-
 ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
 	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
 	args.playerId.showNotification(_U('got_healed'))
