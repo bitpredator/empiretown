a158b01c5b6505d934e7af956d76569048fbe39d
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-09-21 11:27:45 +0200

    fix: esx_basicneeds\server\main.lua fix lint error

--- server-data/resources/[esx_addons]/esx_basicneeds/server/main.lua
+++ server-data/resources/[esx_addons]/esx_basicneeds/server/main.lua
@@ -1,4 +1,4 @@
-ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
+ESX.RegisterCommand('heal', 'admin', function(_, args)
 	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
 	args.playerId.showNotification(_U('got_healed'))
 end, true, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', validate = true, arguments = {
