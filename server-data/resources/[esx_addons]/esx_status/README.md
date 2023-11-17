5292c8162f24a620c5908b10022ef114f5d41c7a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-11-17 21:40:26 +0100

    refactor: remove jquery + Remove some shitty lines

--- server-data/resources/[esx_addons]/esx_status/README.md
+++ server-data/resources/[esx_addons]/esx_status/README.md
@@ -33,5 +33,4 @@ TriggerEvent('esx_status:registerStatus', name, default, color,
 	{remove = 200} -- Client action (add / remove) so the client can be in sync with server
 )
 
-
 ```
--- server-data/resources/[esx_addons]/esx_status/html/scripts/app.js
+++ server-data/resources/[esx_addons]/esx_status/html/scripts/app.js
@@ -1,59 +1,51 @@
 (function () {
 	let status = [];
-
+  
 	let renderStatus = function () {
-
-		$('#status_list').html('');
-
-		for (let i = 0; i < status.length; i++) {
-
-			if (!status[i].visible) {
-				continue;
-			}
-
-			let statusDiv = $(
-				'<div class="status">' +
-					'<div class="status_inner">' +
-						'<div class="status_val"></div>' +
-					'</div>' +
-				'</div>');
-
-			statusDiv.find('.status_inner')
-				.css({ 'border': '1px solid ' + status[i].color })
-				;
-
-			statusDiv.find('.status_val')
-				.css({
-					'background-color': status[i].color,
-					'width': (status[i].val / 10000) + '%'
-				})
-				;
-
-			$('#status_list').append(statusDiv);
+	  document.getElementById("status_list").innerHTML = "";
+  
+	  for (let i = 0; i < status.length; i++) {
+		if (!status[i].visible) {
+		  continue;
 		}
-
+  
+		const statusVal = document.createElement("div");
+		statusVal.style.width = status[i].percent + "%";
+		statusVal.style.backgroundColor = status[i].color;
+		statusVal.classList.add("status_val");
+  
+		const statusInner = document.createElement("div");
+		statusInner.classList.add("status_inner");
+		statusInner.style.border = "1px solid " + status[i].color;
+		statusInner.appendChild(statusVal);
+  
+		const statusDiv = document.createElement("div");
+		statusDiv.classList.add("status");
+		statusDiv.appendChild(statusInner);
+  
+		document.getElementById("status_list").appendChild(statusDiv);
+	  }
 	};
-
+  
 	window.onData = function (data) {
-		if (data.update) {
-			status.length = 0;
-
-			for (let i = 0; i < data.status.length; i++) {
-				status.push(data.status[i]);
-			}
-
-			renderStatus();
+	  if (data.update) {
+		status.length = 0;
+  
+		for (let i = 0; i < data.status.length; i++) {
+		  status.push(data.status[i]);
 		}
-
-		if (data.setDisplay) {
-			$('#status_list').css({ 'opacity': data.display });
-		}
+  
+		renderStatus();
+	  }
+  
+	  if (data.setDisplay) {
+		document.getElementById("status_list").style.opacity = data.display;
+	  }
 	};
-
-	window.onload = function (e) {
-		window.addEventListener('message', function (event) {
-			onData(event.data);
-		});
+  
+	window.onload = function () {
+	  window.addEventListener("message", function (event) {
+		onData(event.data);
+	  });
 	};
-
 })();
