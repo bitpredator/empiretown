74354bc31f85f9d8a4bb00bac8644bf09e28b78f
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-11-24 11:43:31 +0100

    refactor/fix: cleanup, remove useless code, formatting

--- server-data/resources/[esx]/es_extended/client/common.lua
+++ server-data/resources/[esx]/es_extended/client/common.lua
@@ -1,5 +1,4 @@
 AddEventHandler('esx:getSharedObject', function(cb)
-	local Invoke = GetInvokingResource()
 	print(('[^3WARNING^7] ^5%s^7 used ^5esx:getSharedObject^7, this method is deprecated and should not be used! Refer to ^5https://bitpredator.github.io/bptdevelopment/docs/esx-tutorial/sharedevent^7 for more info!'):format(Invoke))
 	cb(ESX)
 end)
--- server-data/resources/[esx]/es_extended/imports.lua
+++ server-data/resources/[esx]/es_extended/imports.lua
@@ -1,36 +1,23 @@
-ESX = exports["es_extended"]:getSharedObject()
+---@diagnostic disable: missing-fields
+ESX = exports['es_extended']:getSharedObject()
 
 if not IsDuplicityVersion() then -- Only register this event for the client
-    AddEventHandler("esx:setPlayerData", function(key, val, last)
-        if GetInvokingResource() == "es_extended" then
+    AddEventHandler('esx:setPlayerData', function(key, val, last)
+        if GetInvokingResource() == 'es_extended' then
             ESX.PlayerData[key] = val
-            if _G.OnPlayerData then
-                _G.OnPlayerData(key, val, last)
-            end
+            -- if OnPlayerData then not used?
+            --     OnPlayerData(key, val, last)
+            -- end
         end
     end)
 
-    AddEventHandler("esx:playerLoaded", function(xPlayer)
+    RegisterNetEvent('esx:playerLoaded', function(xPlayer)
         ESX.PlayerData = xPlayer
         ESX.PlayerLoaded = true
     end)
 
-    AddEventHandler("esx:onPlayerLogout", function()
+    RegisterNetEvent('esx:onPlayerLogout', function()
         ESX.PlayerLoaded = false
         ESX.PlayerData = {}
     end)
-else -- Only register this event for the server
-    local _GetPlayerFromId = ESX.GetPlayerFromId
-    ---@diagnostic disable-next-line: duplicate-set-field
-    function ESX.GetPlayerFromId(playerId)
-        local xPlayer = _GetPlayerFromId(playerId)
-
-        return xPlayer and setmetatable(xPlayer, {
-            __index = function(self, index)
-                if index == "coords" then return self.getCoords() end
-
-                return rawget(self, index)
-            end
-        })
-    end
 end
