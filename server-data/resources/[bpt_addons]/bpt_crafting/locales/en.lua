629f91f84e4a6e75358efa06ad49490ccaebbe09
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-12-19 18:46:27 +0100

    feat:  created the English language

---
+++ server-data/resources/[bpt_addons]/bpt_crafting/locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+ ['not_enough_ingredients'] = 'You don\'t have enough ingredients',
+ ['you_cant_hold_item'] = 'You can\'t create the product',
+ ['item_crafted'] = 'Item crafted!',
+ ['wrong_job'] = 'You cannot open this workbench',
+ ['workbench_hologram'] = '[~b~E~w~] Work station',
+ ['inv_limit_exceed'] = 'Inventory limit exceeded! Remove something before you lose more merchandise',
+ ['crafting_failed'] = 'You were unable to create the item!'
+}
