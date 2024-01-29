394af6f372874e7c0d6511ceed6bfecd9167e97f
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-01-29 18:45:11 +0100

    🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_crafting/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_crafting/fxmanifest.lua
@@ -1,30 +1,30 @@
-fx_version 'adamant'
-versin '1.0.0'
-game 'gta5'
+fx_version("adamant")
+version("1.0.0")
+game("gta5")
 
-shared_script '@es_extended/imports.lua'
+shared_script("@es_extended/imports.lua")
 
-ui_page 'html/form.html'
+ui_page("html/form.html")
 
-files {
-	'html/form.html',
-	'html/css.css',
-	'html/script.js',
-	'html/jquery-3.4.1.min.js',
-	'html/img/*.png',
-}
+files({
+	"html/form.html",
+	"html/css.css",
+	"html/script.js",
+	"html/jquery-3.4.1.min.js",
+	"html/img/*.png",
+})
 
-client_scripts{
-    '@es_extended/locale.lua',
-    'locales/*.lua',
-    'config.lua',
-    'client/main.lua',
-}
+client_scripts({
+	"@es_extended/locale.lua",
+	"locales/*.lua",
+	"config.lua",
+	"client/main.lua",
+})
 
-server_scripts{
-    '@es_extended/locale.lua',
-    'locales/*.lua',
-    '@oxmysql/lib/MySQL.lua',
-    'config.lua',
-    'server/main.lua',
-}
+server_scripts({
+	"@es_extended/locale.lua",
+	"locales/*.lua",
+	"@oxmysql/lib/MySQL.lua",
+	"config.lua",
+	"server/main.lua",
+})
