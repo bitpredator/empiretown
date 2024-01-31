e3a1c9475ebabffbdc3aa0170baba2634f680089
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-01-31 21:01:11 +0100

    🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_unicornjob/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_unicornjob/fxmanifest.lua
@@ -1,23 +1,23 @@
-fx_version 'adamant'
-game 'gta5'
-description 'bpt_unicornjob'
-lua54 'yes'
-version '1.0.0'
+fx_version("adamant")
+game("gta5")
+description("bpt_unicornjob")
+lua54("yes")
+version("1.0.0")
 
-shared_script '@es_extended/imports.lua'
+shared_script("@es_extended/imports.lua")
 
-client_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'client/main.lua'
-}
+client_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "client/main.lua",
+})
 
-server_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'server/main.lua'
-}
+server_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "server/main.lua",
+})
 
-dependency 'es_extended'
+dependency("es_extended")
--- server-data/resources/[bpt_addons]/bpt_unicornjob/locales/it.lua
+++ server-data/resources/[bpt_addons]/bpt_unicornjob/locales/it.lua
@@ -1,32 +1,32 @@
-Locales['it'] = {
-  -- cloakroom
-  ['cloakroom_menu'] = 'guardaroba',
-  ['cloakroom_prompt'] = 'premi [E] per accedere al guardaroba.',
-  ['wear_citizen'] = 'abiti civili',
-  ['wear_work'] = 'abiti da lavoro',
-  -- Inventory
-  ['deposit_stock'] = 'Depositare',
-  ['take_stock'] = 'Prendi',
-  ['have_deposited'] = 'Hai depositato',
-  ['quantity_invalid'] = 'Quantità non valida',
-  -- garage
-  ['spawner_prompt'] = 'premi [E] per accedere al Garage.',
-  ["vehicle_spawned"] = "Veicolo ritirato con successo!",
-  ['store_veh'] = 'premi [E] per depositare il veicolo',
-  ['spawn_veh'] = 'spawn veicolo',
-  ['spawnpoint_blocked'] = 'lo spawnpoint è bloccato !',
-  ['only_unicorn'] = 'puoi solo depositare veicoli aziendali.',
-  ['amount_invalid'] = 'importo non valido',
-  ['press_to_open'] = 'premi [E] per accedere al menu',
-  ['billing'] = 'fattura',
-  ['billing_sent'] = 'la fattura è stata registrata!',
-  ['invoice_amount'] = 'importo della fattura',
-  ['no_players_near'] = 'nessun giocatore nelle vicinanze',
-  ['boss_actions'] = 'Azioni del boss',
-  ['blip_unicorn'] = 'unicorn',
-  ['unicorn'] = 'unicorn',
-  -- billing 
-  ['bill_amount'] = "importo della fattura",
-  ['confirm'] = "Conferma",
-  ['amount'] = "Importo"
+Locales["it"] = {
+    -- cloakroom
+    ["cloakroom_menu"] = "guardaroba",
+    ["cloakroom_prompt"] = "premi [E] per accedere al guardaroba.",
+    ["wear_citizen"] = "abiti civili",
+    ["wear_work"] = "abiti da lavoro",
+    -- Inventory
+    ["deposit_stock"] = "Depositare",
+    ["take_stock"] = "Prendi",
+    ["have_deposited"] = "Hai depositato",
+    ["quantity_invalid"] = "Quantità non valida",
+    -- garage
+    ["spawner_prompt"] = "premi [E] per accedere al Garage.",
+    ["vehicle_spawned"] = "Veicolo ritirato con successo!",
+    ["store_veh"] = "premi [E] per depositare il veicolo",
+    ["spawn_veh"] = "spawn veicolo",
+    ["spawnpoint_blocked"] = "lo spawnpoint è bloccato !",
+    ["only_unicorn"] = "puoi solo depositare veicoli aziendali.",
+    ["amount_invalid"] = "importo non valido",
+    ["press_to_open"] = "premi [E] per accedere al menu",
+    ["billing"] = "fattura",
+    ["billing_sent"] = "la fattura è stata registrata!",
+    ["invoice_amount"] = "importo della fattura",
+    ["no_players_near"] = "nessun giocatore nelle vicinanze",
+    ["boss_actions"] = "Azioni del boss",
+    ["blip_unicorn"] = "unicorn",
+    ["unicorn"] = "unicorn",
+    -- billing
+    ["bill_amount"] = "importo della fattura",
+    ["confirm"] = "Conferma",
+    ["amount"] = "Importo",
 }
