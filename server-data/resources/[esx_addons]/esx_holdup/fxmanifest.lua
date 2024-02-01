2b625fbbf2a040cd8e7129e20e36bb67b021bbc4
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-01 17:17:17 +0100

    🎨 Run formatter

--- server-data/resources/[esx_addons]/esx_holdup/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_holdup/fxmanifest.lua
@@ -1,23 +1,23 @@
-fx_version 'adamant'
+fx_version("adamant")
 
-game 'gta5'
+game("gta5")
 
-description 'ESX Holdup'
+description("ESX Holdup")
 
-version '1.7.5'
+version("1.0.0")
 
-shared_script '@es_extended/imports.lua'
+shared_script("@es_extended/imports.lua")
 
-client_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'client/main.lua'
-}
+client_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "client/main.lua",
+})
 
-server_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
-	'server/main.lua'
-}
+server_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "server/main.lua",
+})
