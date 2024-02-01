946dd56219455e481327a85bf652ee09209ed0a4
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-01 18:14:14 +0100

    🎨 Run formatter

--- server-data/resources/[wasabi]/wasabi_mining/configuration/config.lua
+++ server-data/resources/[wasabi]/wasabi_mining/configuration/config.lua
@@ -1,18 +1,17 @@
 Config = {}
 Config.axe = {
     prop = `prop_tool_pickaxe`, --Default: `prop_tool_pickaxe`
-    breakChance = 20 -- When failing to mine rock, this is the percentage of a chance that your pickaxe will 'break'
+    breakChance = 20, -- When failing to mine rock, this is the percentage of a chance that your pickaxe will 'break'
 }
 
 Config.rocks = { -- Items obtained from mining
-    { item = 'emerald', label = 'Emerald', difficulty = {'easy'} },
-    { item = 'diamond', label = 'Diamond', difficulty = {'easy'} },
-    { item = 'copper', label = 'Copper',  difficulty = {'easy'} },
-    { item = 'iron', label = 'Iron',  difficulty = {'easy'} },
-    { item = 'steel', label = 'Steel',  difficulty = {'easy'} },
-    { item = 'gunpowder', label = 'polvere da sparo', difficulty = {'easy'}},
+    { item = "emerald", label = "Emerald", difficulty = { "easy" } },
+    { item = "diamond", label = "Diamond", difficulty = { "easy" } },
+    { item = "copper", label = "Copper", difficulty = { "easy" } },
+    { item = "iron", label = "Iron", difficulty = { "easy" } },
+    { item = "steel", label = "Steel", difficulty = { "easy" } },
+    { item = "gunpowder", label = "polvere da sparo", difficulty = { "easy" } },
 }
-
 
 Config.miningAreas = {
     vec3(2977.45, 2741.62, 44.62), -- vec3 of locations for mining stones
@@ -21,23 +20,23 @@ Config.miningAreas = {
     vec3(2958.21, 2725.44, 50.16),
     vec3(2946.3, 2725.36, 47.94),
     vec3(3004.01, 2763.27, 43.56),
-    vec3(3001.79, 2791.01, 44.82)
+    vec3(3001.79, 2791.01, 44.82),
 }
 
-RegisterNetEvent('wasabi_mining:notify')
-AddEventHandler('wasabi_mining:notify', function(title, message, msgType)
+RegisterNetEvent("wasabi_mining:notify")
+AddEventHandler("wasabi_mining:notify", function(title, message, msgType)
     -- Place notification system info here, ex: exports['mythic_notify']:SendAlert('inform', message)
     if not msgType then
         lib.notify({
             title = title,
             description = message,
-            type = 'inform'
+            type = "inform",
         })
     else
         lib.notify({
             title = title,
             description = message,
-            type = msgType
+            type = msgType,
         })
     end
 end)
--- server-data/resources/[wasabi]/wasabi_mining/server/server.lua
+++ server-data/resources/[wasabi]/wasabi_mining/server/server.lua
@@ -1,4 +1,4 @@
-lib.callback.register('wasabi_mining:checkPick', function(source, itemname)
+lib.callback.register("wasabi_mining:checkPick", function(source, itemname)
     local item = HasItem(source, itemname)
     if item >= 1 then
         return true
@@ -7,7 +7,7 @@ lib.callback.register('wasabi_mining:checkPick', function(source, itemname)
     end
 end)
 
-lib.callback.register('wasabi_mining:getRockData', function()
+lib.callback.register("wasabi_mining:getRockData", function()
     local data = Config.rocks[math.random(#Config.rocks)]
     return data
 end)
@@ -26,13 +26,13 @@ AddEventHandler("wasabi_mining:mineRock", function(data, index)
         return
     end
     AddItem(source, data.item, 1)
-    TriggerClientEvent('wasabi_mining:notify', source, Strings.rewarded, Strings.rewarded_desc..' '..data.label, 'success')
+    TriggerClientEvent("wasabi_mining:notify", source, Strings.rewarded, Strings.rewarded_desc .. " " .. data.label, "success")
 end)
 
-RegisterServerEvent('wasabi_mining:axeBroke')
-AddEventHandler('wasabi_mining:axeBroke', function()
-    if HasItem(source, 'pickaxe') >= 1 then
-        RemoveItem(source, 'pickaxe', 1)
+RegisterServerEvent("wasabi_mining:axeBroke")
+AddEventHandler("wasabi_mining:axeBroke", function()
+    if HasItem(source, "pickaxe") >= 1 then
+        RemoveItem(source, "pickaxe", 1)
     else
         Wait(2000)
         KickPlayer(source, Strings.kicked)
