675e6c5d319c4c05eccd30e18d6b571845d33d45
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-01 17:43:42 +0100

    🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_wallet/config.lua
+++ server-data/resources/[bpt_addons]/bpt_wallet/config.lua
@@ -1,6 +1,6 @@
 Config = {}
-Config.WalletItem = 'wallet' -- Item name of wallet
+Config.WalletItem = "wallet" -- Item name of wallet
 Config.WalletStorage = {
     slots = 8, -- Slots of wallet storage
-    weight = 10000 -- Total weight for wallet
+    weight = 10000, -- Total weight for wallet
 }
