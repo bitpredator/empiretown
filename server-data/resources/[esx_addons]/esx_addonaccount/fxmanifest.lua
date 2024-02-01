bb05c9d18e89c28bb7d019302ccb273bc56e7cc5
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-01 16:55:47 +0100

    🎨 Run formatter

--- server-data/resources/[esx_addons]/esx_addonaccount/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_addonaccount/fxmanifest.lua
@@ -1,17 +1,17 @@
-fx_version 'adamant'
+fx_version("adamant")
 
-game 'gta5'
+game("gta5")
 
-author 'bitpredaator'
-description 'ESX Addon Account'
+author("bitpredaator")
+description("ESX Addon Account")
 
-version '0.0.4'
+version("1.0.0")
 
-server_scripts {
-	'@es_extended/imports.lua',
-	'@oxmysql/lib/MySQL.lua',
-	'server/classes/addonaccount.lua',
-	'server/main.lua'
-}
+server_scripts({
+    "@es_extended/imports.lua",
+    "@oxmysql/lib/MySQL.lua",
+    "server/classes/addonaccount.lua",
+    "server/main.lua",
+})
 
-dependency 'es_extended'
+dependency("es_extended")
