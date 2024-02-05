6a13a16d9bedef4c873344c334a69eb0557939df
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-05 14:41:52 +0100

    🎨 Run formatter

--- server-data/resources/[esx]/fivem-appearance/configuration/config.lua
+++ server-data/resources/[esx]/fivem-appearance/configuration/config.lua
@@ -195,19 +195,6 @@ Config.ClothingShops = {
             scale = 0.7,
             string = "Clothing Shop",
         },
-        coords = vec3(300.6, -597.7, 42.1), -- Pillbox Hospital
-        distance = 7,
-        price = false,
-    },
-
-    {
-        blip = {
-            enabled = false,
-            sprite = 73,
-            color = 47,
-            scale = 0.7,
-            string = "Clothing Shop",
-        },
         coords = vec3(461.4, -998.0, 30.2), -- MRPD Cloakroom
         distance = 7,
         price = false,
