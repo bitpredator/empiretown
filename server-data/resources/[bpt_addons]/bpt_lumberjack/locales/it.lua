28fad4b35d787a8c92951f68441faea4d16b6bb5
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-28 12:33:15 +0100

    chore: bpt_lumberjack 🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_lumberjack/locales/it.lua
+++ server-data/resources/[bpt_addons]/bpt_lumberjack/locales/it.lua
@@ -1,7 +1,7 @@
-Locales ['it'] = {
-  -- wood
-  ['wood_pickupprompt'] = 'premi ~INPUT_CONTEXT~ per abbattere ~g~l\'albero~s~',
-  ['wood_inventoryfull'] = 'Inventario ~g~pieno~s~.',
-  -- blips
-  ['blip_woodfield'] = 'bosco',
+Locales["it"] = {
+	-- wood
+	["wood_pickupprompt"] = "premi ~INPUT_CONTEXT~ per abbattere ~g~l'albero~s~",
+	["wood_inventoryfull"] = "Inventario ~g~pieno~s~.",
+	-- blips
+	["blip_woodfield"] = "bosco",
 }
--- server-data/resources/[bpt_addons]/bpt_lumberjack/woodcutter.sql
+++ server-data/resources/[bpt_addons]/bpt_lumberjack/woodcutter.sql
@@ -1,5 +1,5 @@
 USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-	('wood', 'Legna', 40, 0, 1)
+	('wood', 'Wood', 40, 0, 1)
 ;
