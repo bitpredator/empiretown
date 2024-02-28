d355a794a1d2d6508697ac276d98a7a29bccb44a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-28 12:20:47 +0100

    chore: bpt_hud fxmanifest updated to version 1.0.1 + Run formatter

--- server-data/resources/[bpt_addons]/bpt_hud/client/status.lua
+++ server-data/resources/[bpt_addons]/bpt_hud/client/status.lua
@@ -6,7 +6,7 @@ local posi = "bottom"
 
 ESX = exports["es_extended"]:getSharedObject()
 
-AddEventHandler('playerSpawned', function()  -- Enable hud only after player spawn
+AddEventHandler("playerSpawned", function() -- Enable hud only after player spawn
 	CreateThread(function()
 		while true do
 			Wait(0)
@@ -16,15 +16,15 @@ AddEventHandler('playerSpawned', function()  -- Enable hud only after player spa
 				health = math.ceil(GetEntityHealth(GetPlayerPed(-1)) - 100)
 			end
 			armor = math.ceil(GetPedArmour(GetPlayerPed(-1)))
-			TriggerEvent('esx_status:getStatus', 'hunger', function(status)
+			TriggerEvent("esx_status:getStatus", "hunger", function(status)
 				food = status.getPercent()
 			end)
-			TriggerEvent('esx_status:getStatus', 'thirst', function(status)
+			TriggerEvent("esx_status:getStatus", "thirst", function(status)
 				thirst = status.getPercent()
 			end)
 			SendNUIMessage({
 				posi = posi,
-				show = IsPauseMenuActive(),  -- Disable hud if settings menu is active
+				show = IsPauseMenuActive(), -- Disable hud if settings menu is active
 				health = health,
 				armor = armor,
 				food = food,
--- server-data/resources/[bpt_addons]/bpt_hud/html-map/script.js
+++ server-data/resources/[bpt_addons]/bpt_hud/html-map/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-window.addEventListener("message", function (event) {
-  let data = event.data;
+  window.addEventListener("message", function (event) {
+    let data = event.data;
 
-  if (data.mapoutline == true) {
-    $(".outline").show();
-  } else if (data.mapoutline == false) {
-    $(".outline").hide();
-  }
+    if (data.mapoutline == true) {
+      $(".outline").show();
+    } else if (data.mapoutline == false) {
+      $(".outline").hide();
+    }
 
-  if (data.mapfoil == true) {
-    $(".mapfoil").hide();
-  } else if (data.mapfoil == false) {
-    $(".mapfoil").show();
-  }
+    if (data.mapfoil == true) {
+      $(".mapfoil").hide();
+    } else if (data.mapfoil == false) {
+      $(".mapfoil").show();
+    }
 
-})})
+  })
+})
