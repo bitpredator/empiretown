05b0b21d893e92e9a50b6d01efb68b0c0587a487
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-28 12:25:31 +0100

    chore: bpt_importjob database update
    - Created database for the Italian language
    - Translated the database into the main directory in English
    - fxmanifest updated to version 1.0.1
    - Improved code formatting

--- server-data/resources/[bpt_addons]/bpt_importjob/server/main.lua
+++ server-data/resources/[bpt_addons]/bpt_importjob/server/main.lua
@@ -1,80 +1,84 @@
-TriggerEvent('esx_society:registerSociety', 'import', 'Import', 'society_import', 'society_import', 'society_import', {
-    type = 'public'
+TriggerEvent("esx_society:registerSociety", "import", "Import", "society_import", "society_import", "society_import", {
+	type = "public",
 })
 
 if Config.MaxInService ~= -1 then
-    TriggerEvent('esx_service:activateService', 'import', Config.MaxInService)
+	TriggerEvent("esx_service:activateService", "import", Config.MaxInService)
 end
 
-ESX.RegisterServerCallback("bpt_importjob:SpawnVehicle", function(source, cb, model , props)
-    local xPlayer = ESX.GetPlayerFromId(source)
+ESX.RegisterServerCallback("bpt_importjob:SpawnVehicle", function(source, cb, model, props)
+	local xPlayer = ESX.GetPlayerFromId(source)
 
-    if xPlayer.job.name ~= "import" then
-        print(('[^3WARNING^7] Player ^5%s^7 attempted to Exploit Vehicle Spawing!!'):format(source))
-        return
-    end
-    local SpawnPoint = vector3(Config.Zones.VehicleSpawnPoint.Pos.x, Config.Zones.VehicleSpawnPoint.Pos.y, Config.Zones.VehicleSpawnPoint.Pos.z)
-    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function()
-        local vehicle = NetworkGetEntityFromNetworkId()
-        while GetVehicleNumberPlateText(vehicle) ~= props.plate do
-            Wait(0)
-        end
-        TaskWarpPedIntoVehicle(GetPlayerPed(source), vehicle, -1)
-    end)
-    cb()
+	if xPlayer.job.name ~= "import" then
+		print(("[^3WARNING^7] Player ^5%s^7 attempted to Exploit Vehicle Spawing!!"):format(source))
+		return
+	end
+	local SpawnPoint = vector3(
+		Config.Zones.VehicleSpawnPoint.Pos.x,
+		Config.Zones.VehicleSpawnPoint.Pos.y,
+		Config.Zones.VehicleSpawnPoint.Pos.z
+	)
+	ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function()
+		local vehicle = NetworkGetEntityFromNetworkId()
+		while GetVehicleNumberPlateText(vehicle) ~= props.plate do
+			Wait(0)
+		end
+		TaskWarpPedIntoVehicle(GetPlayerPed(source), vehicle, -1)
+	end)
+	cb()
 end)
 
-RegisterNetEvent('bpt_importjob:getStockItem')
-AddEventHandler('bpt_importjob:getStockItem', function(itemName, count)
-    local xPlayer = ESX.GetPlayerFromId(source)
+RegisterNetEvent("bpt_importjob:getStockItem")
+AddEventHandler("bpt_importjob:getStockItem", function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
 
-    if xPlayer.job.name == 'import' then
-        TriggerEvent('esx_addoninventory:getSharedInventory', 'society_import', function(inventory)
-            local item = inventory.getItem(itemName)
+	if xPlayer.job.name == "import" then
+		TriggerEvent("esx_addoninventory:getSharedInventory", "society_import", function(inventory)
+			local item = inventory.getItem(itemName)
 
-            -- is there enough in the society?
-            if count > 0 and item.count >= count then
-                -- can the player carry the said amount of x item?
-                if xPlayer.canCarryItem(itemName, count) then
-                    inventory.removeItem(itemName, count)
-                    xPlayer.addInventoryItem(itemName, count)
-                    xPlayer.showNotification(_U('have_withdrawn', count, item.label))
-                else
-                    xPlayer.showNotification(_U('player_cannot_hold'))
-                end
-            else
-                xPlayer.showNotification(_U('quantity_invalid'))
-            end
-        end)
-    else
-        print(('[^3WARNING^7] Player ^5%s^7 attempted ^5bpt_importjob:getStockItem^7 (cheating)'):format(source))
-    end
+			-- is there enough in the society?
+			if count > 0 and item.count >= count then
+				-- can the player carry the said amount of x item?
+				if xPlayer.canCarryItem(itemName, count) then
+					inventory.removeItem(itemName, count)
+					xPlayer.addInventoryItem(itemName, count)
+					xPlayer.showNotification(_U("have_withdrawn", count, item.label))
+				else
+					xPlayer.showNotification(_U("player_cannot_hold"))
+				end
+			else
+				xPlayer.showNotification(_U("quantity_invalid"))
+			end
+		end)
+	else
+		print(("[^3WARNING^7] Player ^5%s^7 attempted ^5bpt_importjob:getStockItem^7 (cheating)"):format(source))
+	end
 end)
 
-ESX.RegisterServerCallback('bpt_importjob:getStockItems', function(_, cb)
-    TriggerEvent('esx_addoninventory:getSharedInventory', 'society_import', function(inventory)
-        cb(inventory.items)
-    end)
+ESX.RegisterServerCallback("bpt_importjob:getStockItems", function(_, cb)
+	TriggerEvent("esx_addoninventory:getSharedInventory", "society_import", function(inventory)
+		cb(inventory.items)
+	end)
 end)
 
-RegisterNetEvent('bpt_importjob:putStockItems')
-AddEventHandler('bpt_importjob:putStockItems', function(itemName, count)
-    local xPlayer = ESX.GetPlayerFromId(source)
+RegisterNetEvent("bpt_importjob:putStockItems")
+AddEventHandler("bpt_importjob:putStockItems", function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
 	local sourceItem = xPlayer.getInventoryItem(itemName)
 
-    if xPlayer.job.name == 'import' then
-        TriggerEvent('esx_addoninventory:getSharedInventory', 'society_import', function(inventory)
-            local item = inventory.getItem(itemName)
+	if xPlayer.job.name == "import" then
+		TriggerEvent("esx_addoninventory:getSharedInventory", "society_import", function(inventory)
+			local item = inventory.getItem(itemName)
 
-            if sourceItem.count >= count and count > 0 then
-                xPlayer.removeInventoryItem(itemName, count)
-                inventory.addItem(itemName, count)
-                xPlayer.showNotification(_U('have_deposited', count, item.label))
-            else
-                xPlayer.showNotification(_U('quantity_invalid'))
-            end
-        end)
-    else
-        print(('[^3WARNING^7] Player ^5%s^7 attempted ^5bpt_importjob:putStockItems^7 (cheating)'):format(source))
-    end
+			if sourceItem.count >= count and count > 0 then
+				xPlayer.removeInventoryItem(itemName, count)
+				inventory.addItem(itemName, count)
+				xPlayer.showNotification(_U("have_deposited", count, item.label))
+			else
+				xPlayer.showNotification(_U("quantity_invalid"))
+			end
+		end)
+	else
+		print(("[^3WARNING^7] Player ^5%s^7 attempted ^5bpt_importjob:putStockItems^7 (cheating)"):format(source))
+	end
 end)
