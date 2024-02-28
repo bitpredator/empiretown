ca3d65823942c9acacb883675c481b46dde55486
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-28 12:37:29 +0100

    chore: bpt_streetfight 🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_streetfight/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_streetfight/fxmanifest.lua
@@ -4,22 +4,22 @@ game("gta5")
 
 description("bpt_streetfight")
 
-version("1.0.0")
+version("1.0.1")
 
 server_script({
-    "@es_extended/locale.lua",
-    "server/server.lua",
-    "locales/*.lua",
-    "config.lua",
+	"@es_extended/locale.lua",
+	"server/server.lua",
+	"locales/*.lua",
+	"config.lua",
 })
 
 client_script({
-    "@es_extended/locale.lua",
-    "client/client.lua",
-    "locales/*.lua",
-    "config.lua",
+	"@es_extended/locale.lua",
+	"client/client.lua",
+	"locales/*.lua",
+	"config.lua",
 })
 
 dependencies({
-    "es_extended",
+	"es_extended",
 })
