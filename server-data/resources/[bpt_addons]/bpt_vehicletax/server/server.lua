fac51d4646c18efed5e4e40b6b60effd4e3c35c1
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-29 01:05:00 +0100

    chore: bpt_vehicletax 🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_vehicletax/server/server.lua
+++ server-data/resources/[bpt_addons]/bpt_vehicletax/server/server.lua
@@ -3,40 +3,47 @@ local percentage = 1
 ESX = exports["es_extended"]:getSharedObject()
 
 MySQL.ready(function()
-    local result = MySQL.Sync.fetchAll("SELECT * FROM vehicles")
-    for i = 1, #result, 1 do
-        if vehicleprices[(GetHashKey(result[i].model))] == nil then
-            table.insert(vehicleprices, { model = GetHashKey(result[i].model), price = result[i].price })
-        end
-    end
+	local result = MySQL.Sync.fetchAll("SELECT * FROM vehicles")
+	for i = 1, #result, 1 do
+		if vehicleprices[(GetHashKey(result[i].model))] == nil then
+			table.insert(vehicleprices, { model = GetHashKey(result[i].model), price = result[i].price })
+		end
+	end
 end)
 
 function Tax()
-    local result = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles")
-    local xPlayers = ESX.GetPlayers()
+	local result = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles")
+	local xPlayers = ESX.GetPlayers()
 
-    for i = 1, #result, 1 do
-        if result[i].job == "" or result[i].job == nil then
-            local sqlplayer = ESX.GetPlayerFromIdentifier(result[i].owner)
+	for i = 1, #result, 1 do
+		if result[i].job == "" or result[i].job == nil then
+			local sqlplayer = ESX.GetPlayerFromIdentifier(result[i].owner)
 
-            if sqlplayer ~= nil then
-                for j = 1, #xPlayers, 1 do
-                    local xPlayer = ESX.GetPlayerFromId(xPlayers[j])
+			if sqlplayer ~= nil then
+				for j = 1, #xPlayers, 1 do
+					local xPlayer = ESX.GetPlayerFromId(xPlayers[j])
 
-                    if xPlayer.identifier == result[i].owner then
-                        local model = json.decode(result[i].vehicle).model
+					if xPlayer.identifier == result[i].owner then
+						local model = json.decode(result[i].vehicle).model
 
-                        for m = 1, #vehicleprices, 1 do
-                            if vehicleprices[m].model == model then
-                                TriggerEvent("esx_billing:sendBill", xPlayer[j], "society_cardealer", _U("vehicle_tax") .. result[i].plate, ((vehicleprices[m].price * percentage) / 100), 1)
-                                break
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
+						for m = 1, #vehicleprices, 1 do
+							if vehicleprices[m].model == model then
+								TriggerEvent(
+									"esx_billing:sendBill",
+									xPlayer[j],
+									"society_cardealer",
+									_U("vehicle_tax") .. result[i].plate,
+									((vehicleprices[m].price * percentage) / 100),
+									1
+								)
+								break
+							end
+						end
+					end
+				end
+			end
+		end
+	end
 end
 
 TriggerEvent("cron", 24, 0, Tax)
