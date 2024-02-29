8628c4e225cbbdbdfd18c7abb60eac34e23a1428
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-02-29 12:27:45 +0100

    chore: pack_cars database creation and code formatting
    - Created database
    - fxmanifest updated to version 1.0.1
    - Improved code formatting

---
+++ server-data/resources/[bpt_cars]/pack_cars.sql
@@ -0,0 +1,3 @@
+INSERT INTO `vehicles` (name, model, price, category) VALUES
+	('Porsche 911 R','p911r',190000,'sports')
+;
--- server-data/resources/[bpt_cars]/tagt3flat6/fxmanifest.lua
+++ server-data/resources/[bpt_cars]/tagt3flat6/fxmanifest.lua
@@ -1,14 +1,15 @@
-fx_version 'adamant'
+fx_version("adamant")
 
-game 'gta5'
+game("gta5")
+author("bitpredator")
 
-files {
-  'audioconfig/tagt3flat6_game.dat151.rel',
-  'audioconfig/tagt3flat6_sounds.dat54.rel',
-  'sfx/dlc_tagt3flat6/tagt3flat6.awc',
-  'sfx/dlc_tagt3flat6/tagt3flat6_npc.awc'
-}
+files({
+	"audioconfig/tagt3flat6_game.dat151.rel",
+	"audioconfig/tagt3flat6_sounds.dat54.rel",
+	"sfx/dlc_tagt3flat6/tagt3flat6.awc",
+	"sfx/dlc_tagt3flat6/tagt3flat6_npc.awc",
+})
 
-data_file 'AUDIO_GAMEDATA' 'audioconfig/tagt3flat6_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/tagt3flat6_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tagt3flat6'
+data_file("AUDIO_GAMEDATA")("audioconfig/tagt3flat6_game.dat")
+data_file("AUDIO_SOUNDDATA")("audioconfig/tagt3flat6_sounds.dat")
+data_file("AUDIO_WAVEPACK")("sfx/dlc_tagt3flat6")
