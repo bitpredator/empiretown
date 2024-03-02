237cb75a8e3959b70af8a165de322b8267e63a11
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-03-02 12:12:54 +0100

    fix: fix error

--- server-data/resources/[sqz]/sqz_carkeys/sql.sql
+++ server-data/resources/[sqz]/sqz_carkeys/sql.sql
@@ -1,4 +1,6 @@
-ALTER TABLE
-    `owned_vehicles`
-ADD
-    COLUMN `peopleWithKeys` LONGTEXT NULL;
+ALTER TABLE `owned_vehicles`
+	ADD COLUMN `peopleWithKeys` LONGTEXT NULL DEFAULT '[]';
+
+-- IF you got errors while executing the query above, use the one bellow
+ALTER TABLE `owned_vehicles`
+	ADD COLUMN `peopleWithKeys` LONGTEXT NULL;
