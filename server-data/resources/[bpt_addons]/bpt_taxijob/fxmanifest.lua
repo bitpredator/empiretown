52ebde60773f4db73bbb743fd15adeeead937376
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-06-16 20:51:20 +0200

    chore: [bpt_addons]\bpt_taxijob 🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_taxijob/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_taxijob/fxmanifest.lua
@@ -1,26 +1,26 @@
-fx_version 'adamant'
+fx_version("adamant")
 
-game 'gta5'
+game("gta5")
 
-author "bitpredator"
-description 'Allows players to be a taxi driver (Pickup and drop-off NPCs)'
-lua54 'yes'
-version '1.0.1'
+author("bitpredator")
+description("Allows players to be a taxi driver (Pickup and drop-off NPCs)")
+lua54("yes")
+version("1.0.1")
 
-shared_script '@es_extended/imports.lua'
+shared_script("@es_extended/imports.lua")
 
-client_scripts {
-    '@es_extended/locale.lua',
-    'locales/*.lua',
-    'config.lua',
-    'client/*.lua'
-}
+client_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "client/*.lua",
+})
 
-server_scripts {
-    '@es_extended/locale.lua',
-    'locales/*.lua',
-    'config.lua',
-    'server/*.lua'
-}
+server_scripts({
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "server/*.lua",
+})
 
-dependency 'es_extended'
+dependency("es_extended")
