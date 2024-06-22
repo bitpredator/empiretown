e5c87ceaf265a1fc89a2874e8b549076c405dcb7
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-06-22 10:25:01 +0200

    chore: update fxmanifest.lua

--- server-data/resources/[maps]/SLBK11_MissionRow/fxmanifest.lua
+++ server-data/resources/[maps]/SLBK11_MissionRow/fxmanifest.lua
@@ -1,13 +1,12 @@
-fx_version 'adamant'
+fx_version("adamant")
+version "1.0.1"
 
-game 'gta5'
+game("gta5")
 
-description 'mission row police map'
+description("mission row police map")
 
-this_is_a_map 'yes'
+this_is_a_map("yes")
 
-client_script {
-    "client/main.lua"
-}
-
-
+client_script({
+	"client/*.lua",
+})
