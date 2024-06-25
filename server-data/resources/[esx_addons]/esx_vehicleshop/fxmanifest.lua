149e98aed27452063b8758f2d0dd93d9e357f539
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-06-25 10:30:57 +0200

    chore: esx_vehicleshop 🎨 Run formatter

--- server-data/resources/[esx_addons]/esx_vehicleshop/fxmanifest.lua
+++ server-data/resources/[esx_addons]/esx_vehicleshop/fxmanifest.lua
@@ -7,18 +7,18 @@ version("1.0.1")
 shared_script("@es_extended/imports.lua")
 
 server_scripts({
-	"@oxmysql/lib/MySQL.lua",
-	"@es_extended/locale.lua",
-	"locales/*.lua",
-	"config.lua",
-	"server/*.lua",
+    "@oxmysql/lib/MySQL.lua",
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "server/*.lua",
 })
 
 client_scripts({
-	"@es_extended/locale.lua",
-	"locales/*.lua",
-	"config.lua",
-	"client/*.lua",
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "client/*.lua",
 })
 
 dependency("es_extended")
