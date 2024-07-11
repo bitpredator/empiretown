efea794f03f27233952471847d2fd755c518a4bc
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-07-11 16:29:09 +0200

    fix: unused global variable GetPlayerCameraRotation

--- server-data/resources/[bpt_addons]/bpt_menu/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_menu/client/main.lua
@@ -157,19 +157,6 @@ function KeyboardInput(entryTitle, textEntry, inputText, maxLength)
     end
 end
 
-function GetPlayerCameraRotation(vector3)
-    local heading = GetGameplayCamRelativeHeading() + GetEntityPhysicsHeading(plyPed)
-    local pitch = GetGameplayCamRelativePitch()
-    local coords = vector3(-math.sin(heading * math.pi / 180.0), math.cos(heading * math.pi / 180.0), math.sin(pitch * math.pi / 180.0))
-    local len = math.sqrt((coords.x * coords.x) + (coords.y * coords.y) + (coords.z * coords.z))
-
-    if len ~= 0 then
-        coords = coords / len
-    end
-
-    return coords
-end
-
 function startAttitude(animSet)
     if not animSet then
         ResetPedMovementClipset(plyPed, 1.0)
