a407e67fd72539a4de356760ff27087d466910d5
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-07-18 11:08:46 +0200

    refactor: check DoesVehicleUseFuel in canInteract

--- server-data/resources/[ox]/ox_fuel/fxmanifest.lua
+++ server-data/resources/[ox]/ox_fuel/fxmanifest.lua
@@ -1,36 +1,37 @@
---[[ FX Information ]]--
-fx_version   'cerulean'
-use_experimental_fxv2_oal 'yes'
-lua54        'yes'
-game         'gta5'
+fx_version("cerulean")
+use_experimental_fxv2_oal("yes")
+lua54("yes")
+game("gta5")
 
---[[ Resource Information ]]--
-name         'ox_fuel'
-author       'Overextended'
-version      '1.4.0'
-repository   'https://github.com/overextended/ox_fuel'
-description  'Fuel management system with ox_inventory support'
+name("ox_fuel")
+author("Overextended")
+version("1.5.1")
+repository("https://github.com/overextended/ox_fuel")
+description("Fuel management system with ox_inventory support")
 
---[[ Manifest ]]--
-dependencies {
-	'ox_lib',
-	'ox_inventory',
-}
+dependencies({
+    "ox_lib",
+    "ox_inventory",
+})
 
-shared_scripts {
-	'@ox_lib/init.lua',
-	'config.lua'
-}
+shared_scripts({
+    "@ox_lib/init.lua",
+    "config.lua",
+})
 
-server_scripts {
-	'server.lua'
-}
+server_scripts({
+    "server.lua",
+})
 
-client_scripts {
-	'data/stations.lua',
-	'client.lua'
-}
+client_script("client/init.lua")
 
-files {
-	'locales/*.json'
-}
+files({
+    "locales/*.json",
+    "data/stations.lua",
+    "client/*.lua",
+})
+
+ox_libs({
+    "math",
+    "locale",
+})
