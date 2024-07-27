f0a59161e9a0e0be2214b38c5dec6f6f012b96a9
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-07-27 20:33:00 +0200

    refactor: localized variables, changed a bit of the code structure.

--- server-data/resources/[bpt_addons]/bpt_billing/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_billing/client/main.lua
@@ -1,40 +1,41 @@
 local isDead = false
 
-function ShowBillsMenu()
+local function showBillsMenu()
     ESX.TriggerServerCallback("bpt_billing:getBills", function(bills)
-        if #bills > 0 then
-            local elements = {
-                { unselectable = true, icon = "fas fa-scroll", title = TranslateCap("invoices") },
-            }
-
-            for _, v in ipairs(bills) do
-                elements[#elements + 1] = {
-                    icon = "fas fa-scroll",
-                    title = ('%s - <span style="color:red;">%s</span>'):format(v.label, TranslateCap("invoices_item", ESX.Math.GroupDigits(v.amount))),
-                    billId = v.id,
-                }
-            end
-
-            ESX.OpenContext("right", elements, function(menu, element)
-                local billId = element.billId
-
-                ESX.TriggerServerCallback("bpt_billing:payBill", function(resp)
-                    if resp == true then
-                        TriggerEvent("bpt_billing:paidBill", billId)
-                    end
-
-                    ShowBillsMenu()
-                end, billId)
-            end)
-        else
-            ESX.ShowNotification(TranslateCap("no_invoices"))
+        if #bills <= 0 then
+            return ESX.ShowNotification(TranslateCap("no_invoices"))
         end
+
+        local elements = {
+            { unselectable = true, icon = "fas fa-scroll", title = TranslateCap("invoices") },
+        }
+
+        for _, v in ipairs(bills) do
+            elements[#elements + 1] = {
+                icon = "fas fa-scroll",
+                title = ('%s - <span style="color:red;">%s</span>'):format(v.label, TranslateCap("invoices_item", ESX.Math.GroupDigits(v.amount))),
+                billId = v.id,
+            }
+        end
+
+        ESX.OpenContext("right", elements, function(menu, element)
+            local billId = element.billId
+
+            ESX.TriggerServerCallback("bpt_billing:payBill", function(resp)
+                showBillsMenu()
+
+                if not resp then
+                    return
+                end
+                TriggerEvent("bpt_billing:paidBill", billId)
+            end, billId)
+        end)
     end)
 end
 
 RegisterCommand("showbills", function()
     if not isDead then
-        ShowBillsMenu()
+        showBillsMenu()
     end
 end, false)
 
@@ -43,6 +44,6 @@ RegisterKeyMapping("showbills", TranslateCap("keymap_showbills"), "keyboard", "F
 AddEventHandler("esx:onPlayerDeath", function()
     isDead = true
 end)
-AddEventHandler("esx:onPlayerSpawn", function(spawn)
+AddEventHandler("esx:onPlayerSpawn", function()
     isDead = false
 end)
--- server-data/resources/[bpt_addons]/bpt_billing/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_billing/fxmanifest.lua
@@ -1,26 +1,22 @@
-fx_version("adamant")
-
+fx_version("cerulean")
 game("gta5")
+lua54("yes")
 
 description("Allows Players to recieve and Send Bills")
-lua54("yes")
-version("1.0.0")
+version("1.0.1")
 
-shared_script("@es_extended/imports.lua")
+shared_scripts({
+    "@es_extended/imports.lua",
+    "@es_extended/locale.lua",
+    "config.lua",
+    "locales/*.lua",
+})
 
 server_scripts({
     "@oxmysql/lib/MySQL.lua",
-    "@es_extended/locale.lua",
-    "locales/*.lua",
-    "config.lua",
     "server/main.lua",
 })
 
-client_scripts({
-    "@es_extended/locale.lua",
-    "locales/*.lua",
-    "config.lua",
-    "client/main.lua",
-})
+client_script("client/main.lua")
 
 dependency("es_extended")
