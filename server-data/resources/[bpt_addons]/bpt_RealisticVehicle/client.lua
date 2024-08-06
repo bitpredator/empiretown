8ecb03a9ff04add0527c224f2cfbe2cdeb18c218
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-08-06 11:56:19 +0200

    convert: esx_RealisticVehicleFailure > bpt_RealisticVehicle

--- server-data/resources/[bpt_addons]/bpt_RealisticVehicle/client.lua
+++ server-data/resources/[bpt_addons]/bpt_RealisticVehicle/client.lua
@@ -58,10 +58,8 @@ local function isPedDrivingAVehicle()
 end
 
 local function fscale(inputValue, originalMin, originalMax, newBegin, newEnd, curve)
-    local OriginalRange = 0.0
+    local OriginalRange, normalizedCurVal, zeroRefCurVal
     local NewRange = 0.0
-    local zeroRefCurVal = 0.0
-    local normalizedCurVal = 0.0
     local rangedValue = 0.0
     local invFlag = 0
 
