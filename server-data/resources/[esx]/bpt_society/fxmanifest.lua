4b276ab33058b0099cc9acdd48691284afb85a30
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-08-12 00:09:09 +0200

    chore: [bpt_addons]\bpt_society 🎨 Run formatter

--- server-data/resources/[esx]/bpt_society/fxmanifest.lua
+++ server-data/resources/[esx]/bpt_society/fxmanifest.lua
@@ -9,20 +9,20 @@ version("1.0.1")
 shared_script("@es_extended/imports.lua")
 
 server_scripts({
-	"@oxmysql/lib/MySQL.lua",
-	"@es_extended/locale.lua",
-	"locales/*.lua",
-	"config.lua",
-	"server/main.lua",
+    "@oxmysql/lib/MySQL.lua",
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "server/main.lua",
 })
 
 client_scripts({
-	"@es_extended/locale.lua",
-	"locales/*.lua",
-	"config.lua",
-	"client/main.lua",
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    "config.lua",
+    "client/main.lua",
 })
 
 dependencies({
-	"es_extended",
+    "es_extended",
 })
