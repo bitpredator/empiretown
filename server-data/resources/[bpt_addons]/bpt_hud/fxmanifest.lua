081bcb6bc73b1d8b922215dd9367e2e14cb524db
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-08-19 11:33:33 +0200

    chore: [bpt_addons]\bpt_hud 🎨 Run formatter

--- server-data/resources/[bpt_addons]/bpt_hud/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_hud/fxmanifest.lua
@@ -3,20 +3,20 @@ fx_version("adamant")
 game("gta5")
 author("bitpredator")
 description("bitpredator HUD")
-version("1.0.1")
+version("1.0.2")
 
 ui_page("html/ui.html")
 
 shared_script("@es_extended/imports.lua")
 
 files({
-	"html/*.css",
-	"html/*.js",
-	"html/*.png",
-	"html/ui.html",
+    "html/*.css",
+    "html/*.js",
+    "html/*.png",
+    "html/ui.html",
 })
 
 client_scripts({
-	"client/*.lua",
-	"config.lua",
+    "client/*.lua",
+    "config.lua",
 })
