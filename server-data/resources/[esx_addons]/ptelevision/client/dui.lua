1a1ba6c1d344c09b9226e010401a7fd0fa3ce028
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-09-17 19:46:11 +0200

    feat:  in-game TV support

---
+++ server-data/resources/[esx_addons]/ptelevision/client/dui.lua
@@ -0,0 +1,44 @@
+function CreateNamedRenderTargetForModel(name, model)
+    local handle = 0
+    if not IsNamedRendertargetRegistered(name) then
+        RegisterNamedRendertarget(name, 0)
+    end
+    if not IsNamedRendertargetLinked(model) then
+        LinkNamedRendertarget(model)
+    end
+    if IsNamedRendertargetRegistered(name) then
+        handle = GetNamedRendertargetRenderId(name)
+    end
+    return handle
+end
+
+function RequestTextureDictionary(dict)
+    RequestStreamedTextureDict(dict)
+    while not HasStreamedTextureDictLoaded(dict) do
+        Wait(0)
+    end
+    return dict
+end
+
+function LoadModel(model)
+    if not IsModelInCdimage(model) then
+        return
+    end
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(0)
+    end
+    return model
+end
+
+function RenderScaleformTV(renderTarget, scaleform, entity)
+    SetTextRenderId(renderTarget) -- set render target
+    Set_2dLayer(4)
+    SetScriptGfxDrawBehindPausemenu(1)
+    --DrawRect(0.5, 0.5, 1.0, 0.5, 255, 0, 0, 255); -- WOAH!
+    local coords = GetEntityCoords(entity)
+    local rot = GetEntityRotation(entity)
+    DrawSprite("ptelevision_b_dict", "ptelevision_b_txd", 0.5, 0.5, 1.0, 1.0, 0.0, 255, 255, 255, 255)
+    SetTextRenderId(GetDefaultScriptRendertargetRenderId()) -- reset
+    SetScriptGfxDrawBehindPausemenu(0)
+end
---
+++ server-data/resources/[esx_addons]/ptelevision/config.lua
@@ -0,0 +1,154 @@
+Config = {}
+
+Config.Models = { -- Any TV Models used on the map or in locations must be defined here.
+    [`des_tvsmash_start`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_flatscreen_overlay`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_laptop_lester2`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_monitor_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_trev_tv_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_03_overlay`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_06`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_01_screen`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_02b`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_03`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_03b`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_michael`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_monitor_w_large`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_03`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+    [`prop_tv_flat_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085,
+        Offset = vector3(-1.02, -0.055, 1.04),
+    },
+}
+
+Config.Locations = { -- REMOVE ALL IF NOT USING ONESYNC, OR IT SHALL BREAK.
+    {
+        Model = `prop_tv_flat_01`,
+        Position = vector4(144.3038, -1037.4647, 29.4173, 70.81),
+    },
+}
+
+Config.Channels = { -- These channels are default channels and cannot be overriden.
+    { name = "Twitch", url = "twitch.tv/twitch" },
+}
+
+Config.BannedWords = {
+    "google",
+}
+
+Config.Events = { -- Events for approving broadcasts / interactions (due to popular demand).
+    ScreenInteract = function(source, data, key, value, cb) -- cb() to approve.
+        if value.url then
+            for i = 1, #Config.BannedWords do
+                if string.find(value.url, Config.BannedWords[i]) then
+                    return
+                end
+            end
+        end
+        cb()
+    end,
+    Broadcast = function(source, data, cb) -- cb() to approve.
+        cb()
+    end,
+}
