f84f37a4d975c14896674b240aca8c85556a6f64
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-09-29 10:43:41 +0200

    fix(server): prevent cascading errors on resource start

--- server-data/resources/[ox]/ox_inventory/server.lua
+++ server-data/resources/[ox]/ox_inventory/server.lua
@@ -1,5 +1,9 @@
 if not lib then return end
 
+require 'modules.bridge.server'
+require 'modules.shops.server'
+require 'modules.pefcl.server'
+
 if GetConvar('inventory:versioncheck', 'true') == 'true' then
 	lib.versionCheck('overextended/ox_inventory')
 end
@@ -8,10 +12,6 @@ local TriggerEventHooks = require 'modules.hooks.server'
 local db = require 'modules.mysql.server'
 local Items = require 'modules.items.server'
 local Inventory = require 'modules.inventory.server'
-
-require 'modules.shops.server'
-require 'modules.pefcl.server'
-require 'modules.bridge.server'
 
 ---@param player table
 ---@param data table?
