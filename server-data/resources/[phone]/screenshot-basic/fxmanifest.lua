44d8a0eb80d01a75c074bb9121e05d643e9a73ed
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-10-07 10:42:53 +0200

    chore: 🎨 Run formatter + fix server.js

--- server-data/resources/[phone]/screenshot-basic/fxmanifest.lua
+++ server-data/resources/[phone]/screenshot-basic/fxmanifest.lua
@@ -1,11 +1,11 @@
-fx_version 'bodacious'
-game 'common'
+fx_version("bodacious")
+game("common")
 
-client_script 'dist/client.js'
-server_script 'dist/server.js'
+client_script("dist/client.js")
+server_script("dist/server.js")
 
-files {
-    'dist/ui.html'
-}
+files({
+    "dist/ui.html",
+})
 
-ui_page 'dist/ui.html'
+ui_page("dist/ui.html")
