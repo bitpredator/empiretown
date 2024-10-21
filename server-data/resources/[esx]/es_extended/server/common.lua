868ffa14a1d3bbced294133d29768a369e9e0a52
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-10-21 10:58:14 +0200

    fix: fixed link to getSharedObject documentation

--- server-data/resources/[esx]/es_extended/server/common.lua
+++ server-data/resources/[esx]/es_extended/server/common.lua
@@ -20,7 +20,7 @@ end)
 
 AddEventHandler("esx:getSharedObject", function()
     local Invoke = GetInvokingResource()
-    print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://bitpredator.github.io/bptdevelopment/docs/esx-tutorial/sharedevent for how to fix!"):format(Invoke))
+    error(("Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://bitpredator.github.io/bptdevelopment/docs/FiveM/sharedevent/ for how to fix!"):format(Invoke))
 end)
 
 exports("getSharedObject", function()
