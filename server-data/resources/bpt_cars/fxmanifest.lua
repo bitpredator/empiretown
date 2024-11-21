5e797051383e2410e4152ac46f41b4bd254de10f
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-11-21 22:21:54 +0100

    feat: push pack cars addon

---
+++ server-data/resources/bpt_cars/fxmanifest.lua
@@ -0,0 +1,58 @@
+fx_version("adamant")
+game("gta5")
+description("bpt_cars - pack addon car")
+author("bitpredator")
+lua54("yes")
+version("1.0.2")
+
+files({
+    -- bmw440
+    "data/bmw440/handling.meta",
+    "data/bmw440/vehicles.meta",
+    "data/bmw440/carcols.meta",
+    "data/bmw440/carvariations.meta",
+
+    -- bmwm3e92
+    "data/bmwm3e92/handling.meta",
+    "data/bmwm3e92/vehicles.meta",
+    "data/bmwm3e92/carcols.meta",
+    "data/bmwm3e92/carvariations.meta",
+
+    -- SkylineGT-R
+    "data/SkylineGT-R/handling.meta",
+    "data/SkylineGT-R/vehicles.meta",
+    "data/SkylineGT-R/carcols.meta",
+    "data/SkylineGT-R/carvariations.meta",
+
+    -- mads14
+    "data/mads14/handling.meta",
+    "data/mads14/vehicles.meta",
+    "data/mads14/carvariations.meta",
+})
+
+-- bmw440
+data_file("HANDLING_FILE")("data/bmw440/handling.meta")
+data_file("VEHICLE_METADATA_FILE")("data/bmw440/vehicles.meta")
+data_file("CARCOLS_FILE")("data/bmw440/carcols.meta")
+data_file("VEHICLE_VARIATION_FILE")("data/bmw440/carvariations.meta")
+
+-- bmwm3e92
+data_file("HANDLING_FILE")("data/bmwm3e92/handling.meta")
+data_file("VEHICLE_METADATA_FILE")("data/bmwm3e92/vehicles.meta")
+data_file("CARCOLS_FILE")("data/bmwm3e92/carcols.meta")
+data_file("VEHICLE_VARIATION_FILE")("data/bmwm3e92/carvariations.meta")
+
+-- SkylineGT-R
+data_file("HANDLING_FILE")("data/SkylineGT-R/handling.meta")
+data_file("VEHICLE_METADATA_FILE")("data/SkylineGT-R/vehicles.meta")
+data_file("CARCOLS_FILE")("data/SkylineGT-R/carcols.meta")
+data_file("VEHICLE_VARIATION_FILE")("data/SkylineGT-R/carvariations.meta")
+
+-- mads14
+data_file("HANDLING_FILE")("data/mads14/handling.meta")
+data_file("VEHICLE_METADATA_FILE")("data/mads14/vehicles.meta")
+data_file("VEHICLE_VARIATION_FILE")("data/mads14/carvariations.meta")
+
+client_script({
+    "vehicle_names.lua",
+})
---
+++ server-data/resources/bpt_cars/vehicle_names.lua
@@ -0,0 +1,6 @@
+CreateThread(function()
+    AddTextEntry("440i", "BMW 440")
+    AddTextEntry("bmwm3e92", "BMW M3e92")
+    AddTextEntry("skyline", "SkylineGT-R")
+    AddTextEntry("mads14", "Nissan S14 Drift")
+end)
