cf3941e6f937dfb8dc317df2f49e3191dcc094a1
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-12-07 14:38:14 +0100

    fix:  Fixed invisible belt icon issue

---
+++ server-data/resources/[esx_addons]/seatbelt/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version("adamant")
+game("gta5")
+author("bitpredator")
+lua54("yes")
+version("1.0.2")
+
+client_script "main.lua"
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/app.js',
+	'html/style.css',
+	'img/seatbelt.png'
+}
