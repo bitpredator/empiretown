e224cb98fe06728fff17099b86bd382192ef2d3a
bitpredator <67551273+bitpredator@users.noreply.github.com>
2024-12-07 10:51:52 +0100

    refactor:  hud reconstruction
    In this PR a lot of reconstruction work has been done for the hud, now it will be shown through a small symbol and percentages, the position has been moved to the left under the minimap (minimap which will only be activated when you are on a vehicle) .

---
+++ server-data/resources/[esx_addons]/FLX_hud/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version("adamant")
+game("gta5")
+
+description("bpt_RealisticVehicle")
+lua54("yes")
+version("1.0.2")
+
+shared_script("@es_extended/imports.lua")
+
+client_scripts({
+    "client.lua",
+})
