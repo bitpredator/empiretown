2239fc569359c4a3dcfd739861e1d9c8b6a3540e
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-02-24 18:17:31 +0100

    fix:  (sqz_carkeys) corrected importing of translations

--- server-data/resources/[sqz]/sqz_carkeys/fxmanifest.lua
+++ server-data/resources/[sqz]/sqz_carkeys/fxmanifest.lua
@@ -7,15 +7,15 @@ version("1.0.8")
 
 shared_scripts({
     "@es_extended/locale.lua",
-    "locales/*.lua",
     "@es_extended/imports.lua",
     "config.lua",
+    "locales/*.lua",
 })
 
 client_scripts({
     "client/client.lua",
-    "locales/*.lua",
 })
+
 server_scripts({
     "@oxmysql/lib/MySQL.lua",
     "server/server.lua",
