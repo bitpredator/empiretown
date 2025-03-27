f696475c46d0f7b4bd2616e76af8da06b4916cb7
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-03-27 18:46:59 +0100

    chore: removed queue and whitelist system

--- server-data/resources/[bpt_addons]/bpt-queue/modules/connection/client.lua
+++
@@ -1,9 +0,0 @@
-CreateThread(function()
-    while true do
-        Wait(1)
-        if NetworkIsSessionStarted() then
-            TriggerServerEvent("queue:playerInitiated")
-            return
-        end
-    end
-end)
