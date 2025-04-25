9418bd86509e14346f16560fbc9db34c733f0335
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-04-25 11:17:07 +0200

    chore: improved formatting + minor fixes

--- server-data/resources/[managers]/spawnmanager/fxmanifest.lua
+++ server-data/resources/[managers]/spawnmanager/fxmanifest.lua
@@ -1,12 +1,12 @@
 -- This resource is part of the default Cfx.re asset pack (cfx-server-data)
 -- Altering or recreating for local use only is strongly discouraged.
 
-version '1.0.0'
-author 'Cfx.re <root@cfx.re>'
-description 'Handles spawning a player in a unified fashion to prevent resources from having to implement custom spawn logic.'
-repository 'https://github.com/citizenfx/cfx-server-data'
+version("1.0.0")
+author("Cfx.re <root@cfx.re>")
+description("Handles spawning a player in a unified fashion to prevent resources from having to implement custom spawn logic.")
+repository("https://github.com/citizenfx/cfx-server-data")
 
-client_script 'spawnmanager.lua'
+client_script("spawnmanager.lua")
 
-fx_version 'adamant'
-games {'gta5' }
+fx_version("adamant")
+games({ "gta5" })
