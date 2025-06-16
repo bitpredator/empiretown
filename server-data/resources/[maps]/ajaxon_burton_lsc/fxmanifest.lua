958729ff7f830f1ffc53de6d0eb171635af44366
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-06-16 10:24:13 +0200

    Update fxmanifest.lua
    A custom high-quality MLO map of the iconic ajaxon_burton_lsc for FiveM. Includes YMAP, YDR, YBN, YTYP, and other necessary map files. Optimized for seamless integration and immersive roleplay environments.

--- server-data/resources/[maps]/ajaxon_burton_lsc/fxmanifest.lua
+++ server-data/resources/[maps]/ajaxon_burton_lsc/fxmanifest.lua
@@ -1,11 +1,19 @@
-fx_version "adamant"
-game 'gta5'
-this_is_a_map "yes"
-version '0.0.5'
+fx_version("adamant")
+game("gta5")
 
-files {
-    "interiorproxies.meta"
-}
+this_is_a_map("yes")
 
+version("2.0.0")
+description("Mappa MLO Ajaxon Burton LSC")
+author("BPTNetwork")
 
-data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+files({
+    "stream/**/*.ytyp",
+    "stream/**/*.ydr",
+    "stream/**/*.ymap",
+    "stream/**/*.ybn",
+    "stream/**/*.ymf",
+    "interiorproxies.meta",
+})
+
+data_file("INTERIOR_PROXY_ORDER_FILE")("interiorproxies.meta")
