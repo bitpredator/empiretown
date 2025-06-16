dafa1decf8f5685ce5088afcb1a6e0309ddc7799
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-06-16 11:02:13 +0200

    gabz_mrpd + benny's MLO - FiveM Map
    A custom high-quality MLO map of the iconic for FiveM. Includes YMAP, YDR, YBN, YTYP, and other necessary map files. Optimized for seamless integration and immersive roleplay environments.

--- server-data/resources/[maps]/bennys/fxmanifest.lua
+++ server-data/resources/[maps]/bennys/fxmanifest.lua
@@ -1,4 +1,16 @@
 fx_version("adamant")
 game("gta5")
+
 this_is_a_map("yes")
-version("1.0.8")
+
+version("2.0.0")
+description("Mappa MLO bennys")
+author("BPTNetwork")
+
+files({
+    "stream/**/*.ytyp",
+    "stream/**/*.ydr",
+    "stream/**/*.ymap",
+    "stream/**/*.ybn",
+    "stream/**/*.ymf",
+})
