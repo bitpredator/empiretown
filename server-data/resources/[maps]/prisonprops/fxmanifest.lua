4f4016505a1831743eba6367a2a792f5f74f31d2
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-07-09 11:56:52 +0200

    fix: (fix stylua check) prisonprops\fxmanifest.lua

--- server-data/resources/[maps]/prisonprops/fxmanifest.lua
+++ server-data/resources/[maps]/prisonprops/fxmanifest.lua
@@ -1,6 +1,15 @@
-fx_version("adamant")
+fx_version("cerulean")
 game("gta5")
-this_is_a_map("yes")
-version("1.0.8")
-data_file('DLC_ITYP_REQUEST')('stream/prison_props.ytyp')
 
+author("BPTNetwork")
+description("Prison props for EmpireTownRP")
+version("2.0.0")
+this_is_a_map("yes")
+
+files({
+    "stream/*.yft",
+    "stream/*.ydr",
+    "stream/*.ytd",
+    "stream/*.ymf",
+})
+data_file("DLC_ITYP_REQUEST")("stream/prison_props.ytyp")
