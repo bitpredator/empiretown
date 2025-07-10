929b766465acd09051a3f24a5eef4cbe8053c9fc
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-07-10 09:34:49 +0200

    Dynamic Chat Suggestions for FiveM (wx.Suggestions)
    A lightweight utility that registers dynamic chat command suggestions in FiveM using chat:addSuggestion, based on the structured wx.Suggestions table. Clean, modular, and easy to extend.

--- server-data/resources/[esx_addons]/wx_rpchat/client/suggestions.lua
+++ server-data/resources/[esx_addons]/wx_rpchat/client/suggestions.lua
@@ -1,9 +1,14 @@
-Citizen.CreateThread(function()
-    for command, options in pairs(wx.Suggestions) do
-        for k,v in pairs(options.argument) do
-            TriggerEvent('chat:addSuggestion', command, options.description,  { { name = k, help = v } } )
+CreateThread(function()
+    for command, options in pairs(wx.Suggestions or {}) do
+        local args = {}
+
+        for name, help in pairs(options.argument or {}) do
+            table.insert(args, {
+                name = name,
+                help = help,
+            })
         end
+
+        TriggerEvent("chat:addSuggestion", command, options.description or "", args)
     end
 end)
-
--- @todo: rewrite. works, but could be done better
