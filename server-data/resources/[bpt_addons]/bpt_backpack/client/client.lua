bb621fee246e6627b9ad367b275614c9e3e0bcfb
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-08-02 12:08:42 +0200

    chore: Improved code formatting + updated Discord links

--- server-data/resources/[bpt_addons]/bpt_backpack/client/client.lua
+++ server-data/resources/[bpt_addons]/bpt_backpack/client/client.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 local bagEquipped, bagObj
 local hash = `p_michael_backpack_s`
 local ox_inventory = exports.ox_inventory
@@ -5,7 +6,7 @@ local ped = cache.ped
 local justConnect = nil
 
 local function PutOnBag()
-    local x, y, z = table.unpack(GetOffsetFromEntityInWorldCoords(ped,0.0,3.0,0.5))
+    local x, y, z = table.unpack(GetOffsetFromEntityInWorldCoords(ped, 0.0, 3.0, 0.5))
     lib.requestModel(hash, 100)
     bagObj = CreateObjectNoOffset(hash, x, y, z, true, false, false)
     AttachEntityToEntity(bagObj, ped, GetPedBoneIndex(ped, 24818), 0.07, -0.11, -0.05, 0.0, 90.0, 175.0, true, true, false, true, 1, true)
@@ -21,22 +22,22 @@ local function RemoveBag()
     bagEquipped = nil
 end
 
-AddEventHandler('ox_inventory:updateInventory', function(changes)
+AddEventHandler("ox_inventory:updateInventory", function(changes)
     if justConnect then
         Wait(4500)
         justConnect = nil
     end
     for _, v in pairs(changes) do
-        if type(v) == 'table' then
-            local count = ox_inventory:Search('count', 'backpack')
-	        if count > 0 and (not bagEquipped or not bagObj) then
+        if type(v) == "table" then
+            local count = ox_inventory:Search("count", "backpack")
+            if count > 0 and (not bagEquipped or not bagObj) then
                 PutOnBag()
             elseif count < 1 and bagEquipped then
                 RemoveBag()
             end
         end
-        if type(v) == 'boolean' then
-            local count = ox_inventory:Search('count', 'backpack')
+        if type(v) == "boolean" then
+            local count = ox_inventory:Search("count", "backpack")
             if count < 1 and bagEquipped then
                 RemoveBag()
             end
@@ -44,16 +45,18 @@ AddEventHandler('ox_inventory:updateInventory', function(changes)
     end
 end)
 
-lib.onCache('ped', function(value)
+lib.onCache("ped", function(value)
     ped = value
 end)
 
-lib.onCache('vehicle', function(value)
-    if GetResourceState('ox_inventory') ~= 'started' then return end
+lib.onCache("vehicle", function(value)
+    if GetResourceState("ox_inventory") ~= "started" then
+        return
+    end
     if value then
         RemoveBag()
     else
-        local count = ox_inventory:Search('count', 'backpack')
+        local count = ox_inventory:Search("count", "backpack")
         if count and count >= 1 then
             PutOnBag()
         end
@@ -63,9 +66,9 @@ end)
 exports("openBackpack", function(data, slot)
     if not slot?.metadata?.identifier then
         local identifier = lib.callback.await("bpt_backpack:getNewIdentifier", 100, data.slot)
-        ox_inventory:openInventory("stash", "bag_"..identifier)
+        ox_inventory:openInventory("stash", "bag_" .. identifier)
     else
         TriggerServerEvent("bpt_backpack:openBackpack", slot.metadata.identifier)
-        ox_inventory:openInventory("stash", "bag_"..slot.metadata.identifier)
+        ox_inventory:openInventory("stash", "bag_" .. slot.metadata.identifier)
     end
 end)
