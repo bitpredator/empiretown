40b5e3c5f2b69767a1bf45268263e88147b5b06b
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-08-06 10:39:04 +0200

    chore: Improved code formatting + updated Discord links

--- server-data/resources/[bpt_addons]/bpt_basicItem/client/main.lua
+++ server-data/resources/[bpt_addons]/bpt_basicItem/client/main.lua
@@ -30,8 +30,7 @@ AddEventHandler("weapon:USE", function()
     closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
     if closestDistance ~= -1 and closestDistance <= 2.0 then
         ESX.ShowNotification((playerReceiveCard):format(GetPlayerName(closestPlayer)))
-        TriggerServerEvent("bpt_idcard:open", GetPlayerServerId(PlayerId()), GetPlayerServerId(closestPlayer),
-            "weapon")
+        TriggerServerEvent("bpt_idcard:open", GetPlayerServerId(PlayerId()), GetPlayerServerId(closestPlayer), "weapon")
     else
         TriggerServerEvent("bpt_idcard:open", GetPlayerServerId(PlayerId()), GetPlayerServerId(PlayerId()), "weapon")
     end
--- server-data/resources/[bpt_addons]/bpt_basicItem/fxmanifest.lua
+++ server-data/resources/[bpt_addons]/bpt_basicItem/fxmanifest.lua
@@ -3,7 +3,7 @@ game("gta5")
 
 description("fivem basic Item")
 author("bitpredator")
-version("1.0.8")
+version("2.0.0")
 lua54("yes")
 
 shared_scripts({
