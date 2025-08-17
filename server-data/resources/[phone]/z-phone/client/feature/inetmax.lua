429d23df03b2680c98a41962dcae4b068c8608f0
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-08-17 18:53:01 +0200

    fixed issue with social media posts not being created

--- server-data/resources/[phone]/z-phone/client/feature/inetmax.lua
+++ server-data/resources/[phone]/z-phone/client/feature/inetmax.lua
@@ -1,26 +1,28 @@
-RegisterNUICallback('get-internet-data', function(_, cb)
-    lib.callback('z-phone:server:GetInternetData', false, function(result)
+RegisterNUICallback("get-internet-data", function(_, cb)
+    lib.callback("z-phone:server:GetInternetData", false, function(result)
         cb(result)
     end)
 end)
 
-RegisterNUICallback('topup-internet-data', function(body, cb)
+RegisterNUICallback("topup-internet-data", function(body, cb)
     if not IsAllowToSendOrCall() then
         TriggerEvent("z-phone:client:sendNotifInternal", {
             type = "Notification",
             from = Config.App.InetMax.Name,
-            message = Config.MsgSignalZone
+            message = Config.MsgSignalZone,
         })
         cb(false)
         return
     end
-    
-    lib.callback('z-phone:server:TopupInternetData', false, function(purchaseInKB)
-        Profile.inetmax_balance = Profile.inetmax_balance + purchaseInKB
+
+    lib.callback("z-phone:server:TopupInternetData", false, function(purchaseInKB)
+        Profile = Profile or {}
+        Profile.inetmax_balance = (Profile.inetmax_balance or 0) + purchaseInKB
         cb(purchaseInKB)
     end, body)
 end)
 
-RegisterNetEvent('z-phone:client:usage-internet-data', function(app, usageInKB)
-    Profile.inetmax_balance = Profile.inetmax_balance - usageInKB
+RegisterNetEvent("z-phone:client:usage-internet-data", function(app, usageInKB)
+    Profile = Profile or {}
+    Profile.inetmax_balance = (Profile.inetmax_balance or 0) - usageInKB
 end)
